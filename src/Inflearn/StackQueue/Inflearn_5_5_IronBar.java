15ee2a871d46435d4e13344e8fa806fa42078c51
kgh2120 <kgh2120@naver.com>
2022-05-02 10:13:56 +0900

    5-5 쇠막대기 문제 해결

--- src/Inflearn/StackQueue/Inflearn_5_5_IronBar.java
+++ src/Inflearn/StackQueue/Inflearn_5_5_IronBar.java
@@ -1,7 +1,13 @@
+package Inflearn.StackQueue;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
 
-public class Main {
+/**
+ * 정답률이 굉장히 높았는데, 푸는데 너무 오래 걸렸던 문제.
+ * 규칙을 찾아내는데 처음에 잘못 생각해서 너무 헤메었다.
+ */
+public class Inflearn_5_5_IronBar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String con = sc.next();
--- src/Inflearn/TTEst.java
+++ src/Inflearn/TTEst.java
@@ -1,9 +1,24 @@
 package Inflearn;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class TTEst {
     public static void main(String[] args) {
 
-//        System.out.println(Integer.valueOf(a+"") + Integer.valueOf('2'+""));
+        List<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+
+
+
+        Optional<Integer> reduce = l.stream().reduce((max, integer) -> {
+            return integer > max ? integer : 0;
+        });
+
+        System.out.println(reduce.get());
     }
 
 }
