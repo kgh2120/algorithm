7828a3ee0e62174c0c423e5b54c72281564e8c95
kgh2120 <kgh2120@naver.com>
2022-05-10 14:22:52 +0900

    6-8 뮤직비디오 해결

--- src/Inflearn/Sorting/Inflearn_6_8_MusicVideo.java
+++ src/Inflearn/Sorting/Inflearn_6_8_MusicVideo.java
@@ -1,7 +1,57 @@
-import java.util.*;
+package Inflearn.Sorting;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Inflearn_6_8_MusicVideo {
+    public static void submit(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nOfNum = sc.nextInt();
+        int target = sc.nextInt();
+
+        int total = 0;
+        int[] arr = new int[nOfNum];
+        for (int i = 0; i < nOfNum; i++) {
+            int num = sc.nextInt();
+            total += num;
+            arr[i] = num;
+        }
+        int avg = total / target;
+        int minimumDVD = submitGetMinimumDVD(arr, target, avg);
+        System.out.println(minimumDVD);
+
+    }
+
+    private static int submitGetMinimumDVD(int[]arr, int target, int amount) {
+        int loop = 0;
+        int val = 0;
+        for (int i = 0; i < arr.length; i++) {
+            val += arr[i];
+
+            if (val == amount) {
+                val = 0;
+                loop++;
+                continue;
+            }
+            if (i == arr.length - 1) {
+                loop++;
+                break;
+            }
+            if (val + arr[i+1] > amount) {
+                val = 0;
+                loop++;
+            }
+        }
+
+        if(loop > target)
+            return submitGetMinimumDVD(arr,target,amount+1);
+        else
+            return amount;
+
+    }
+
+
+    public static void solution(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nOfNum = sc.nextInt();
         int target = sc.nextInt();
