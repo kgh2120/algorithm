5f0425cce1bfdd1fa63ed917552bfc5d8c49f316
kyoohyun <kgh2120@naver.com>
2022-07-04 09:43:24 +0900

    webhook test2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!!");
+        System.out.println("test2");
     }
 
 }
