948f951c1fc6e14894b706eb09d967f281b8dc61
kyoohyun <kgh2120@naver.com>
2022-07-04 09:40:46 +0900

    webhook test

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int[][] memo;
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int r = sc.nextInt();
-
-        memo = new int[n+1][r+1];
-
-        System.out.println(combinationMemo(n, r));
-
+        System.out.println("hello world!!");
     }
 
-    public static int combination(int n, int r) {
-        if(n==r) return 1;
-        if(r==0) return 1;
-
-        return combination(n-1,r-1) + combination(n-1,r);
-    }
-
-    public static int combinationMemo(int n, int r) {
-        if (n == r || r==0) {
-            if(memo[n][r]==0)
-                memo[n][r] = 1;
-            return memo[n][r];
-        }
-
-        if(memo[n-1][r-1] == 0)
-            memo[n-1][r-1] = combinationMemo(n-1,r-1);
-        if (memo[n-1][r]==0)
-            memo[n-1][r] = combinationMemo(n-1,r);
-
-        return memo[n-1][r-1] + memo[n-1][r];
-    }
 }
