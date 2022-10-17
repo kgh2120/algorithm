5848df365d860de23ae75392f359669e27edbee7
kyoohyun <kgh2120@naver.com>
2022-10-17 11:46:12 +0900

    백준 정수론 파트 문제 풀기 - 1037 - 1934 - 2609 - 2981 - 5086

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,7 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-
-    static BufferedWriter bw;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String val = br.readLine();
-        String[] cons = val.split(" ");
-        int x = Integer.parseInt(cons[0]);
-        int y = Integer.parseInt(cons[1]);
-
-        Backtracking(x,y,1,0,"");
-        bw.flush();
-    }
-
-    public static void Backtracking(int x, int y,int last, int now, String result) throws IOException {
-        if (y == now) {
-            bw.write(result+"\n");
-        }else {
-            for (int i = 1; i <=x ; i++) {
-                if(i >= last)
-                    Backtracking(x,y,i,now+1,result+i+ " ");
-            }
-        }
-    }
 
 
 }
