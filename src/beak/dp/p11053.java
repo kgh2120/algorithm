4ae870e3ad4aea32fb94b1663d00bd605309956e
kyoohyun <kgh2120@naver.com>
2022-11-07 11:15:30 +0900

    221107 동적 계획법 문제 풀이

--- src/beak/dp/p11053.java
+++ src/beak/dp/p11053.java
@@ -1,9 +1,11 @@
+package beak.dp;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class p11053 {
 
     int []arr;
     int[]dp;
@@ -49,7 +51,7 @@ public class Main {
 
 
     public static void main(String []args) throws Exception {
-        new Main().solution();
+        new p11053().solution();
     }
 }
 
