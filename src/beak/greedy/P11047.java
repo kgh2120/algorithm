92d5507eb9d488fb1cb57fa19e3759a641d4f399
kyoohyun <kgh2120@naver.com>
2022-12-30 11:53:43 +0900

    221230 그리디 알고리즘 문제 풀이

---
+++ src/beak/greedy/P11047.java
@@ -0,0 +1,42 @@
+package beak.greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11047 {
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int nOfCoin = Integer.parseInt(st.nextToken());
+        int limit = Integer.parseInt(st.nextToken());
+        Integer[] coins = new Integer[nOfCoin];
+        for (int i = 0; i < nOfCoin; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+
+        int result = 0;
+        for (int i = nOfCoin-1; i >= 0; i--) {
+            int coin = coins[i];
+            if(limit >= coin){
+                result += limit/coin;
+                limit = limit%coin;
+            }
+        }
+        System.out.println(result);
+    }
+
+
+    public static void main(String []args) throws Exception {
+        new P11047().solution();
+    }
+}
+
+
+
+
+
--- src/beak/greedy/P1931.java
+++ src/beak/greedy/P1931.java
@@ -1,10 +1,23 @@
+package beak.greedy;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 그리디 알고리즘의 예시로 자주 나오는 회의실 배정 문제.
+ * 핵심은 이전 선택이 다음 선택에 영향을 주지 않는다는 것 이었다.
+ * 이 문제에서 이전 선택이 다음 선택에 영향을 주지 않는 기준은
+ * 회의 A의 종료 시간이 회의 B의 시작 시간보다 앞이여야 한다는 점이다.
+ * 이 조건 하에, 빠르게 종료되는 회의들을 선택하면, 가장 많은 회의를 배정할 수 있는 것이다.
+ * 그래서 빠르게 종료되는 회의를 기준으로 정렬을 진행하였다.
+ * 하지만 이 과정에서 회의 시작에 대한 정렬을 해주지 않아서 한번 틀렸다.
+ * 생각을 하긴 했는데, 알아서 해주는 줄 알았다.
+ * 항상 반례를 잘 고민해야겠다.
+ */
+public class P1931 {
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,7 +54,7 @@ public class Main {
 
 
     public static void main(String []args) throws Exception {
-        new Main().solution();
+        new P1931().solution();
     }
 }
 
