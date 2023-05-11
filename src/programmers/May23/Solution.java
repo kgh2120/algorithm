fa9544bf0d1192e6f7b383726443fc5243c5388c
kyoohyun <kgh2120@naver.com>
2023-05-11 21:26:39 +0900

    solve programmers l2 181188

---
+++ src/programmers/May23/Solution.java
@@ -0,0 +1,28 @@
+package programmers.May23;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, new Comparator<int[]>(){
+            public int compare(int[]o1, int[] o2){
+                if(o1[1]==o2[1])
+                    return o1[0] - o2[0];
+                return o1[1]-o2[1];
+            };
+        });
+        int end = targets[0][1];
+        answer ++;
+        for(int target[] : targets){
+            if(target[0] >= end){
+                end = target[1];
+                answer++;
+            }
+        }
+
+
+        return answer;
+    }
+}
