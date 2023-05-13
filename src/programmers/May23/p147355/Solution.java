106a08bb28a3bebdea3e8e5d55051a317b285553
kyoohyun <kgh2120@naver.com>
2023-05-13 23:16:19 +0900

    solve programmers l1 p147355

---
+++ src/programmers/May23/p147355/Solution.java
@@ -0,0 +1,39 @@
+package programmers.May23.p147355;
+
+class Solution {
+    // sliding window 하면 어떨까?
+    public int solution(String t, String p) {
+
+        int answer = 0;
+
+        int length = p.length();
+        int l = 0;
+        int r = length;
+
+        long target = Long.parseLong(p);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i <r; i++)
+            sb.append(t.charAt(i));
+
+        long f = Long.parseLong(sb.toString());
+        if(f <= target)
+            answer++;
+
+
+
+        while(r <t.length()){
+            sb.deleteCharAt(0);
+            sb.append(t.charAt(r++));
+            f = Long.parseLong(sb.toString());
+
+            if(f <= target)
+                answer++;
+        }
+
+
+
+
+        return answer;
+    }
+}
