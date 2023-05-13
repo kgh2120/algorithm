4c2454eb52b1f17e6b0a2ffb0f414b553bcd72a7
kyoohyun <kgh2120@naver.com>
2023-05-14 08:17:40 +0900

    solve programmers l1 p135808

---
+++ src/programmers/May23/p135808/Solution.java
@@ -0,0 +1,22 @@
+package programmers.May23.p135808;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        // score 큰 순으로 정렬해서 박스로 묶어서 판매하면 될 듯 ?
+        Arrays.sort(score);
+        int count = 0;
+        for(int i = score.length-1; i>= 0; i--){
+            count++;
+            if(count == m){
+                int s = score[i];
+                answer += s * m;
+                count = 0;
+            }
+        }
+
+        return answer;
+    }
+}
