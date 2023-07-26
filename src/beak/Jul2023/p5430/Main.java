c6797764b812a013eb7c21d8f60ae44483c60525
kyoohyun <kgh2120@gmail.com>
2023-07-26 12:07:04 +0900

    solve boj g5 p5430

--- src/beak/Jul2023/p5430/Main.java
+++ src/beak/Jul2023/p5430/Main.java
@@ -1,7 +1,8 @@
+package beak.Jul2023.p5430;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
