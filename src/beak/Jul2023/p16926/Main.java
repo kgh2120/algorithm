3926a419d625742d72a7cf21bbba3444aabe33e5
kyoohyun <kgh2120@naver.com>
2023-07-30 19:13:48 +0900

    solve boj s1 p16926

--- src/beak/Jul2023/p16926/Main.java
+++ src/beak/Jul2023/p16926/Main.java
@@ -1,5 +1,8 @@
-import java.io.*;
-import java.util.*;
+package beak.Jul2023.p16926;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
