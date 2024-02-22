f160c580abd3de83ef5258cc614774327d869f42
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-02-23 08:25:18 +0900

    [Gold V] Title: 신촌 통폐합 계획, Time: 700 ms, Memory: 169360 KB -BaekjoonHub

--- 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
+++ 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
@@ -20,16 +20,15 @@ public class Main {
             nodes[i] = new Node(br.readLine());
         }
 
+        int index = 0;
+
         for (int i = 0; i < n-1; i++) {
             st = new StringTokenizer(br.readLine());
             int fromIndex = Integer.parseInt(st.nextToken());
             int toIndex = Integer.parseInt(st.nextToken());
-
-
             if(nodes[fromIndex] == null || nodes[toIndex] == null) continue;
             Node from = nodes[fromIndex];
             Node to = nodes[toIndex];
-
             if (from.tail == null) {
                 if (to.tail == null) {
                     from.tail = to;
@@ -49,17 +48,13 @@ public class Main {
                 }
             }
             nodes[toIndex] = null;
+            index = fromIndex;
         }
-
-        int index;
-        for (index = 0; index < n; index++) {
-            if(nodes[index] != null)
-                break;
-        }
-
-
-//        System.out.println(Arrays.toString(nodes));
-
+//        int index;
+//        for (index = 0; index < n; index++) {
+//            if(nodes[index] != null)
+//                break;
+//        }
         for(Node node = nodes[index]; node != null; node = node.next)
             sb.append(node.value);
 
@@ -74,15 +69,6 @@ public class Main {
         Node tail;
         public Node(String value) {
             this.value = value;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "value='" + value + '\'' +
-                    ", next=" + next +
-                    ", tail=" + tail +
-                    '}';
         }
     }
 
