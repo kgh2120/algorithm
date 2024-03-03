75142a8bc0655e505e3f7b69f76a2043ac820b4d
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-03-03 15:01:03 +0900

    [Gold V] Title: 동전 1, Time: 100 ms, Memory: 11908 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,57 +1,49 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * - @author 규현
+ * - @since 2024-03-03
+ * - @limit memory : 4mb time : 0.5ms
+ * - @performance
+ * - @category #DP
+ * - @note
+ */
 public class Main {
 
-	static int k,n;
-	static int[]arr;
-	static int[][]memo;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// k+1의 memo를 만들고, 각 위치에서 n으로 이동한 경우만큼 +1 하기..? 같은 것을 어떻게 걸러줘야 하지?
-		// 그럼 for문에서 i를 넣어주는 식으로 하면..
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		arr = new int[n];
-		memo = new int[k+1][n];
-		for(int i = 0; i<n; i++) {
-			int m =Integer.parseInt(br.readLine());
-			arr[i] = m;
-		}
-		Arrays.sort(arr);
-		
-		for(int i = 0; i<n; i++) {
-			int m = arr[i];
-			if(m > k)
-				break;
-			memo[m][i]++;
-		}
-		comb(1);
-			
-		int total = 0;
-		for(int data : memo[k])
-			total += data;
-		System.out.println(total);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
 
-		
-	}
-	
-	private static void comb(int weight) {
-		if(weight >= k)
-			return;
-		
-		for(int i = 0; i<n; i++) {
-			for(int j = i; j<n; j++) {
-				if(weight + arr[j] > k)
-					break;
-				memo[weight + arr[j]][j] += memo[weight][i];
-			}
-		}
-		comb(weight+1);
-			
-	}
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[k + 1];
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int number= Integer.parseInt(br.readLine());
+            if(number <= k)
+                numbers.add(number);
+        }
+
+        Collections.sort(numbers, Collections.reverseOrder());
+
+
+        for (Integer number : numbers) {
+            arr[number]++;
+            for (int i = number; i <= k ; i++) {
+                arr[i] += arr[i-number];
+            }
+        }
+
+        System.out.println(arr[k]);
+
+
+    }
+
+
+
+
 }
