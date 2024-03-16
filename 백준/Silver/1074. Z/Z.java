f3d5ea39a0bba11654f4b86d944299bf4885dcbd
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-03-16 16:42:42 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,68 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
 /**
-    @author 김규현
-    @since 2023-08-14
-    @see
-    @git
-    @youtube
-    @performance
-    @category #
-    @note
-
-*/
+ * - @author 규현
+ * - @since 2024-03-16
+ * - @limit memory :  time :
+ * - @performance
+ * - @category
+ * - @note
+ */
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int n, r,c;
+    static StringBuilder sb = new StringBuilder();
+
+
+
     public static void main(String[] args) throws Exception {
 
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int targetR = Integer.parseInt(st.nextToken());
+        int targetC = Integer.parseInt(st.nextToken());
 
-        recursive(0,0,0,n);
+        System.out.println(findValue(0, 0, 0, targetR, targetC, n));
 
     }
 
-    private static void recursive(int startValue, int sr, int sc, int n){
-        // r과 c가 몇 사분면에 속하는 지 체크하고 startValue를 넘긴다
-
-        if (sr == r && sc == c) {
-            System.out.println(startValue);
-            return;
-        }
+    static int findValue(int value, int startR, int startC, int targetR, int targetC, int n){
         if(n == 0)
-            return;
-        int nValue = (int)Math.pow(2, 2 * (n-1));
+            return value;
 
-        int size = (int) Math.pow(2, n-1);
-//        if(sr + size >= r)
-        // 1사분면
-        if (sr + size > r && sc + size > c) {
-            recursive(startValue, sr,sc,n-1);
+        int a = (int) Math.pow(2, n - 1);
+        int b = (int) Math.pow(4, n-1);
+
+        if (targetR >= startR + a) {
+            value += b*2;
+            startR += a;
         }
-        // 2사분면
-        if (sr + size > r && sc + size <= c) {
-            recursive(startValue + nValue, sr,sc+size,n-1);
+        if (targetC >= startC + a) {
+            value += b;
+            startC += a;
         }
-        // 3사분면
-        if (sr + size <= r && sc + size > c) {
-            recursive(startValue + nValue * 2, sr+size,sc,n-1);
-        }
-        // 4
-        if (sr + size <= r && sc + size <= c) {
-            recursive(startValue + nValue * 3, sr+size,sc+size,n-1);
-        }
+        return findValue(value, startR, startC, targetR, targetC, n-1);
+
 
     }
+
 
 
 
