8e7e6d5563cb2b00c960f5d97e75f427d4b6234c
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-03-26 08:30:08 +0900

    [Gold III] Title: 음악프로그램, Time: 96 ms, Memory: 12104 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.java
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -1,112 +1,91 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-
-
-/**
-
-@author 규현
-@since 2023-09-10
-@url
-@level
-@try
-@performance
-@category #
-@note
- 남일이는 보조 피디들이 가져온 순서에 맞춰서 전체 가수들의 순서를 작성해야한다.
- 피디들은 자기만의 순서를 가져오고 이 모든 순서를 만족하는 경우를 찾아야 함.
-
- 이때 경우를 찾지 못하면 -1을 출력해야 함.
-
- 위상 정렬 알고리즘을 이용해야 하는데,
-
-*/
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
+    static Graph graph;
 
-    static Node[] graph;
-    static int[] counts;
-
-    static int n, nOfPD;
+    static int[] childCount;
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        nOfPD = Integer.parseInt(st.nextToken());
+        int size = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        graph = new Node[n+1]; // 자기 다음꺼 받아서 간선으로 넣어줌
-        counts = new int[n+1];
+        graph = new Graph(size);
+        childCount = new int[size+1];
 
-        for (int i = 0; i < nOfPD; i++) {
+        for(int i = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
+            int listSize = Integer.parseInt(st.nextToken());
 
-            int before = Integer.parseInt(st.nextToken());
+            if(listSize <= 1)
+                continue;
 
-            // 인접 리스트 구성
-            for (int j = 1; j < k; j++) {
-                int cur = Integer.parseInt(st.nextToken());
-                graph[before] = new Node(cur, graph[before]);
-                before = cur;
-                counts[cur]++;
+            int from = Integer.parseInt(st.nextToken());
+            for(int j = 1; j<listSize; j++){
+                int to = Integer.parseInt(st.nextToken());
+                graph.addEdge(from,to);
+                childCount[to]++;
+                from = to;
             }
         }
 
-        topologySort();
-        for (int i = 1; i <= n; i++) {
-            if (counts[i] != 0) {
-                System.out.println(0);
-                return;
-            }
-        }
-
-        System.out.println(sb);
-
-
-    }
-
-    private static void topologySort(){
         Queue<Integer> q = new ArrayDeque<>();
-        for (int i = 1; i <= n ; i++) {
-            if(counts[i] == 0)
+
+        for(int i = 1; i<= size; i++){
+            if(childCount[i] == 0)
                 q.add(i);
         }
 
-        while (!q.isEmpty()) {
-            Integer poll = q.poll();
-            append(poll);
-            for (Node node = graph[poll]; node != null; node = node.next) {
-                counts[node.value]--;
-                if (counts[node.value] == 0) {
-                    q.add(node.value);
+        while(!q.isEmpty()){
+            int poll = q.poll();
+            sb.append(poll).append("\n");
+
+            for(Edge e = graph.getEdge(poll); e != null; e = e.next){
+                if(--childCount[e.to] == 0){
+                    q.add(e.to);
                 }
             }
         }
+        
+        for(int i = 1; i<= size; i++)
+            if(childCount[i] != 0){
+                System.out.println(0);
+                return;
+            }
+        
+        System.out.println(sb);
+    }
+
+    static class Graph{
+
+        Edge [] edges;
+
+        public Graph(int size){
+            edges = new Edge[size+1];
+        }
+
+        public void addEdge(int from, int to){
+            edges[from] = new Edge(to, edges[from]);
+        }
+
+        public Edge getEdge(int from){
+            return edges[from];
+        }
 
     }
 
-    private static void append(Integer poll) {
-        sb.append(poll).append("\n");
-    }
+    static class Edge{
+        int to;
+        Edge next;
 
-    static class Node{
-        int value;
-        Node next;
-
-        public Node(int value, Node next) {
-            this.value = value;
+        public Edge(int to, Edge next){
+            this.to = to;
             this.next = next;
         }
     }
-
-
-
-
 }
