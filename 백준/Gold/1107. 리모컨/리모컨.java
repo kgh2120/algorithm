bcd600f32e27d8fb83457db361d0ebc2fb93725b
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-05-23 17:54:28 +0900

    [Gold V] Title: 리모컨, Time: 2352 ms, Memory: 299172 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,70 +1,63 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
+class Main {
 
-/**
- * - @author 규현
- * - @since 2024-03-16
- * - @limit memory :  time :
- * - @performance
- * - @category
- * - @note
- */
-public class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static List<Integer> numbers;
+    static boolean[] isBroken;
     static int target;
-
     static int min;
-
-
+    static int length;
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        length = s.length();
+        target = Integer.parseInt(s);
+        int nOfBroken = Integer.parseInt(br.readLine());
+        isBroken = new boolean[10];
 
-        target = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
-        numbers = new ArrayList<>();
-        numbers.addAll(Arrays.asList(0, 1,2,3,4,5,6,7,8,9));
-
-        if (n != 0) {
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                numbers.remove(Integer.valueOf(st.nextToken()));
-            }
+        if (nOfBroken != 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens())
+                isBroken[Integer.parseInt(st.nextToken())] = true;
         }
+
+
+
 
         min = Math.abs(target - 100);
-        createNumber(new StringBuilder(), 0);
-        System.out.println(min);
-    }
-
-    static void createNumber(StringBuilder word, int depth){
-        if (depth == 7) {
-
-            if (word.length() == 0) {
-                return;
-            }
-
-            int num = Integer.parseInt(word.toString());
-
-            min  = Math.min(min, calcDistance(num));
+        if (min == 0) {
+            System.out.println(min);
             return;
         }
-        createNumber(word, depth+1);
-        for (Integer number : numbers) {
-            word.append(number);
-            createNumber(word, depth+1);
-            word.deleteCharAt(word.length()-1);
+        findShortest(0,"");
+        System.out.println(min);
+
+    }
+
+    private static void findShortest( int depth, String current){
+
+
+        if(current.length() - length > 1)
+            return;
+        if(depth != 0){
+            int count = Math.abs(Integer.parseInt(current) - target) + current.length();
+            min = Math.min(min,count);
         }
-    }
 
-    static int calcDistance(int number){
-        return Integer.toString(number).length() + Math.abs(number - target);
-    }
+        // 종료조건을 어떻게 잡아야 하지...
+        // 자리수가 1개 차이나는 거 까지만 허용해야 할 듯.. 2개 넘어가면 멈추도록 해야 할 듯 하다.
 
+        // 일단은 돌아가는 로직 부터
+
+        for (int i = 0; i < 10; i++) {
+
+            if(isBroken[i])
+                continue;
+            findShortest(depth+1, current+i);
+        }
+
+    }
 
 
 
