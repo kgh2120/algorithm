d0bb53b0f6c3fcf0161974bd9624a2fa19cc7f01
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-06-17 17:42:46 +0900

    [Silver II] Title: 과일 탕후루, Time: 136 ms, Memory: 15604 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,20 +1,21 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int[] countArray = new int[10];
-    static int nOfKinds = 0;
+    static PScanner scanner  = new PScanner(System.in);
+
 
     public static void main(String[] args) throws Exception {
 
-        int n = Integer.parseInt(br.readLine());
+        int n = scanner.nextInt();
         int[] tanghuru = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
-            tanghuru[i] = Integer.parseInt(st.nextToken());
+            tanghuru[i] = scanner.nextInt();
         }
 
         int left = 0;
@@ -63,5 +64,109 @@ public class Main {
 
 
 
+    public static class PScanner {
+        private final InputStreamReader in;
+        private final char[] buf;
+        private final char[] cbuf;
+        private int len, ptr;
 
+        public PScanner(InputStream input) {
+            in = new InputStreamReader(input);
+            buf = new char[8192];
+            cbuf = new char[8192];
+        }
+
+        public boolean hasNext() {
+            consume();
+            return ptr < len && buf[ptr] > ' ';
+        }
+
+        public boolean hasNextInLine() {
+            char c;
+            while ((c = read()) <= ' ' && c != 0 && c != '\n') ;
+            ptr--;
+            return ptr < len && buf[ptr] > ' ';
+        }
+
+        public String next() {
+            consume();
+            char[] cbuf = this.cbuf;
+            int clen = 0;
+            int start = ptr;
+            while (true) {
+                if (ptr < len) {
+                    if (buf[ptr++] <= ' ') {
+                        cbuf = copy(buf, start, cbuf, clen, ptr - start);
+                        clen += ptr - start;
+                        break;
+                    }
+                } else if (ptr == len) {
+                    cbuf = copy(buf, start, cbuf, clen, ptr - start);
+                    clen += ptr - start;
+                    fill();
+                    start = ptr;
+                } else break;
+            }
+            ptr--;
+            if (ptr < len) clen--;
+            return new String(cbuf, 0, clen);
+        }
+
+        private char[] copy(char[] src, int srcPos, char[] dest, int destPos, int length) {
+            if (dest.length < destPos + length) dest = Arrays.copyOf(dest, dest.length << 1);
+            System.arraycopy(src, srcPos, dest, destPos, length);
+            return dest;
+        }
+
+        public char nextChar() {
+            consume();
+            return read();
+        }
+
+        public int nextInt() {
+            consume();
+            int v = 0;
+            char c = read();
+            boolean neg = c == '-';
+            if (neg) c = read();
+            do {
+                v = v * 10 + c - '0';
+            } while ('0' <= (c = read()) && c <= '9');
+            ptr--;
+            return neg ? -v : v;
+        }
+
+        public long nextLong() {
+            consume();
+            long v = 0;
+            char c = read();
+            boolean neg = c == '-';
+            if (neg) c = read();
+            do {
+                v = v * 10 + c - '0';
+            } while ('0' <= (c = read()) && c <= '9');
+            ptr--;
+            return neg ? -v : v;
+        }
+
+        private char read() {
+            if (ptr == len) fill();
+            return ptr < len ? buf[ptr++] : 0;
+        }
+
+        private void fill() {
+            try {
+                len = in.read(buf);
+                ptr = 0;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void consume() {
+            char c;
+            while ((c = read()) <= ' ' && c != 0) ;
+            ptr--;
+        }
+    }
 }
