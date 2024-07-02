22abd6c04ae23511c50850209142ffbe7920b31d
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-07-02 09:40:45 +0900

    [Gold V] Title: 물통, Time: 80 ms, Memory: 20552 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -1,107 +1,85 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-/*
-    2251 물통
-    물통에 담긴 물을 다른 물통으로 옮겨 담는다.
-    물을 옮길 때의 규칙은 A -> B로 옮긴다고 했을 때, A의 양이 0이 되거나 B의 양이 최대로 되는 경우까지 물을 옮긴다.
-
-    물통은 3개 A,B,C가 있고, 시작하면 A와 B는 물이 없고, C에만 물이 최대로 차있다.
-
-    여러 경우가 존재할 때, A 물통에는 물이 없고, C에만 물이 있는 경우에서 C에 물이 얼마가 있는지 경우를 모두 나열하라는 문제이다.
-
-    특별한 방법이 있는것 같지는 않고, BFS같이 돌면서 그 상황을 저장하면서 경우를 봐야겠다. 그런데 visited 체크를 해줘야 하는데
-    이걸 딱히 생각이 안나니까 3차원 boolean 배열을 만들어서 A,B,C의 물 용량칸이 true면 visited로 생각해보자.
- */
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
 
-    static int a;
-    static int b;
-    static int c;
+    static public void main(String []args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static boolean[][][] visited;
-    static  int[] max;
-    static Queue<int[]> q;
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws Exception {
+        boolean [][][]visited = new boolean[a+1][b+1][c+1];
+        int[]maxValue = {a,b,c};
 
-        st = new StringTokenizer(br.readLine());
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        max = new int[]{a, b, c};
-
-        visited = new boolean[a+1][b+1][c+1];
-
-        q = new ArrayDeque<>();
-        int[] init = {0, 0, c};
-        q.add(init);
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(new int[]{0,0,c}));
+        Set<Integer> answers = new HashSet<>();
+        answers.add(c);
         visited[0][0][c] = true;
-        Set<Integer> set = new HashSet<>();
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
+            Node node = q.poll();
 
-            int[] poll = q.poll();
-            if (poll[0] == 0) {
-//                System.out.println(Arrays.toString(poll));
-                set.add(poll[2]);
+            int[] value = node.value;
+            for (int i = 0; i < 3; i++) {
+                if(value[i] == 0) continue;
+                for (int j = 0; j < 3; j++) {
+                    if(i == j ||  value[j] == maxValue[j]) continue;
+                    // i -> j로 이동  i는 i - maxValue - value[j]
+                    int[] copy = copy(value);
+                    water(i,j,copy,maxValue);
+                    if(isVisited(copy, visited)) continue;
+                    visited[copy[0]][copy[1]][copy[2]] = true;
+                    if(copy[0] == 0){
+                        answers.add(copy[2]);
+                    }
+                    q.add(new Node(copy));
+                }
             }
 
-
-            int calc = calc(poll, 0, 1);
-            if (!visited[poll[0] - calc][poll[1] + calc][poll[2]]) {
-                q.add(new int[]{poll[0] - calc, poll[1] + calc, poll[2]});
-                visited[poll[0] - calc][poll[1] + calc][poll[2]] = true;
-            }
-
-            int calc2 = calc(poll, 0, 2);
-            if (!visited[poll[0] - calc][poll[1]][poll[2] +  calc2]) {
-                q.add(new int[]{poll[0] - calc2, poll[1], poll[2] +  calc2});
-                visited[poll[0] - calc][poll[1]][poll[2] +  calc2] = true;
-            }
-
-            int calc3 = calc(poll, 1, 0);
-            if (!visited[poll[0] + calc3][poll[1] - calc3][poll[2]]) {
-                q.add(new int[]{poll[0] + calc3, poll[1]- calc3, poll[2]});
-                visited[poll[0] + calc3][poll[1] - calc3][poll[2]] = true;
-            }
-
-            int calc4 = calc(poll, 1, 2);
-            if (!visited[poll[0]][poll[1] - calc4][poll[2] + calc4]) {
-                q.add(new int[]{poll[0], poll[1]- calc4, poll[2] + calc4});
-                visited[poll[0]][poll[1] - calc4][poll[2] + calc4] = true;
-            }
-
-            int calc5 = calc(poll, 2, 0);
-            if (!visited[poll[0] + calc5][poll[1]][poll[2] - calc5]) {
-                q.add(new int[]{poll[0] + calc5,poll[1],poll[2] - calc5});
-                visited[poll[0] + calc5][poll[1]][poll[2] - calc5] = true;
-            }
-
-            int calc6 = calc(poll, 2, 1);
-            if (!visited[poll[0]][poll[1] + calc6][poll[2] - calc6]) {
-                q.add(new int[]{poll[0],poll[1] + calc6,poll[2] - calc6});
-                visited[poll[0]][poll[1] + calc6][poll[2] - calc6] = true;
-            }
         }
 
-        List<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
 
         StringBuilder sb = new StringBuilder();
-        for (Integer i : list) {
+        List<Integer> answer = new ArrayList<>(answers);
+        Collections.sort(answer);
+        for (Integer i : answer) {
             sb.append(i).append(" ");
         }
         System.out.println(sb);
 
 
     }
-    static int calc(int[] poll, int from, int to){
-        return Math.min(poll[from], max[to] - poll[to]);
+    static boolean isVisited(int[]cups, boolean [][][]visited){
+        return visited[cups[0]][cups[1]][cups[2]];
     }
+
+    static int[] copy(int[] origin){
+        int[] copy = new int[origin.length];
+        System.arraycopy(origin,0, copy, 0, origin.length);
+        return copy;
+    }
+
+    static void water(int from, int to, int[] cups, int[] maxValues) {
+        int fromValue = cups[from];
+        int toValue = cups[to];
+
+        cups[from] = Math.max(0, fromValue - (maxValues[to] - toValue));
+        cups[to] = Math.min(fromValue + toValue, maxValues[to]);
+
+    }
+
+    static class Node{
+        int[] value;
+
+        public Node(int[] value) {
+            this.value = value;
+        }
+    }
+
 
 }
