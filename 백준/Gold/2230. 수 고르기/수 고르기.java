8b974e5bb1d1d66d02135a898bec8ff8b98fc433
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-07-03 13:04:33 +0900

    [Gold V] Title: 수 고르기, Time: 272 ms, Memory: 28244 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -19,58 +19,31 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             int number = Integer.parseInt(br.readLine());
             arr[i] = number;
-            set.add(number);
-        }
 
-        boolean isDup = false;
-        if (arr.length != set.size()) {
-            isDup = true;
         }
-
-        if (isDup && m == 0) {
-            System.out.println(0);
-            return;
-        }
-
         Arrays.sort(arr);
-
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            int left = arr[i];
 
-            int target = left - m;
+        int l = 0;
+        int r = 0;
 
-
-            int targetIndex = Arrays.binarySearch(arr, target);
-            if (targetIndex < 0) {
-                targetIndex = -(targetIndex + 2);
-            }
-
-            if(targetIndex >= n || targetIndex < 0)
-                continue;
-
-            int right = arr[targetIndex];
-            if (left == right) {
-                if (targetIndex == 0) {
-                    right = arr[targetIndex+1];
-                } else if (targetIndex == n - 1) {
-                    right = arr[targetIndex-1];
-                }else
-                    right = Math.min(arr[targetIndex+1], arr[targetIndex-1]);
-            }
-
-            int diff = Math.abs(right - left);
-            if(diff >= m)
-                min = Math.min(diff, min);
+        while (l <= r && r < n) {
+            int diff = Math.abs(arr[r] - arr[l]);
+            if (diff >= m) {
+                min = Math.min(min, diff);
+                l++;
+            }else
+                r++;
         }
         System.out.println(min);
 
 
     }
+
+
 
 
 }
