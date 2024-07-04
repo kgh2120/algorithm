477015c10897b9c9443ee4994f28f7df3349bb43
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-07-04 10:32:29 +0900

    [Silver I] Title: 스티커, Time: 552 ms, Memory: 114468 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -16,19 +16,18 @@ public class Main {
 
         for (int t = 0; t < T; t++) {
             int n = Integer.parseInt(br.readLine());
-            int[][] matrix = new int[2][n];
             int[][] dp = new int[2][n+1];
 
             StringTokenizer st1 = new StringTokenizer(br.readLine());
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             for (int i = 0; i < n; i++) {
-                matrix[0][i] = Integer.parseInt(st1.nextToken());
-                matrix[1][i] = Integer.parseInt(st2.nextToken());
+                int upper = Integer.parseInt(st1.nextToken());
+                int lower = Integer.parseInt(st2.nextToken());
 
-                dp[0][i + 1] = Math.max(dp[1][i] + matrix[1][i], dp[0][i]); 
-                dp[1][i + 1] = Math.max(dp[1][i], dp[0][i] + matrix[0][i]); 
+                dp[0][i + 1] = Math.max(dp[1][i] +lower, dp[0][i]);
+                dp[1][i + 1] = Math.max(dp[1][i], dp[0][i] + upper);
             }
-            
+
             int answer = Math.max(dp[0][n], dp[1][n]);
             sb.append(answer).append("\n");
         }
