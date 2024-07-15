8c6e9cf35e74853d9d4e9e4e02d89a17c8d1ad2a
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-07-15 22:53:06 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 660 ms, Memory: 64412 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -16,36 +16,30 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n+1];
+        int[] start = new int[n+1];
+        int[] end =  new int[n+1];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Queue<Work> startQueue = new PriorityQueue<>((w1,w2) -> Integer.compare(w1.start,w2.start));
-        Queue<Work> endQueue = new PriorityQueue<>((w1,w2) -> Integer.compare(w1.end,w2.end));
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            Work work = new Work(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            startQueue.add(work);
-
+            int startIdx = Integer.parseInt(st.nextToken());
+            int endIdx = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            start[startIdx] += value;
+            end[endIdx] += value;
         }
 
         int curValue = 0;
         int idx = 1;
         while (idx <= n) {
             // st가 있으면 check
-            while (!startQueue.isEmpty() && startQueue.peek().start == idx) {
-                Work poll = startQueue.poll();
-                curValue += poll.value;
-                endQueue.add(poll);
-
-            }
+            curValue += start[idx];
             arr[idx] += curValue;
-
-            while(!endQueue.isEmpty() && endQueue.peek().end == idx) {
-                curValue -= endQueue.poll().value;
-            }
+            curValue -= end[idx];
             idx++;
         }
 
@@ -55,23 +49,6 @@ public class Main {
             sb.append(arr[i]).append(" ");
         }
         System.out.println(sb);
-
-
-
     }
-
-    static class Work{
-        int start;
-        int end;
-        int value;
-
-        public Work(int start, int end, int value) {
-            this.start = start;
-            this.end = end;
-            this.value = value;
-        }
-    }
-
-
-
+    
 }
