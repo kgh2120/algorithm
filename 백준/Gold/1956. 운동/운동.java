d9ccbf3b62dc2c6143558e56b2f84538106371d8
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-07-23 21:10:54 +0900

    [Gold IV] Title: 운동, Time: 572 ms, Memory: 58412 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Main {
 
-    static Edge[] graph;
+
     static int[] dist;
     static final int INF = 10_0000_0000;
 
@@ -19,9 +19,11 @@ public class Main {
         int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
 
-        graph = new Edge[v + 1];
-        dist = new int[v + 1];
+        int[][] matrix = new int[v+1][v+1];
 
+        for (int[] ints : matrix) {
+            Arrays.fill(ints, INF);
+        }
 
 
         for (int i = 0; i < e; i++) {
@@ -29,68 +31,25 @@ public class Main {
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int weg = Integer.parseInt(st.nextToken());
-
-            graph[from] = new Edge(to, weg, graph[from]);
+            matrix[from][to] = weg;
         }
 
-        for (int i = 1; i <= v ; i++) {
-            Arrays.fill(dist, INF);
-            dijkstra(i);
-            min = Math.min(min, dist[i]);
-        }
-
-        System.out.println(min == INF? -1 : min );
-
-
-
-    }
-
-    static void dijkstra(int index){
-
-        Queue<Edge> pq = new PriorityQueue<>();
-//        dist[index] = 0;
-        pq.add(new Edge(index, 0, null));
-
-
-
-        while (!pq.isEmpty()) {
-            Edge edge = pq.poll();
-
-            if(dist[edge.to] < edge.weight) continue;
-
-            for(Edge e = graph[edge.to]; e != null; e = e.next){
-
-//                if (e.to == index) {
-//                    return edge.weight + e.weight;
-//                }
-
-                if (dist[e.to] > edge.weight + e.weight) {
-                    dist[e.to] = edge.weight + e.weight;
-                    pq.add(new Edge(e.to, dist[e.to], null));
+        for (int k = 1; k <= v ; k++) {
+            for (int i = 1; i <= v ; i++) {
+                for (int j = 1; j <= v ; j++) {
+                    if(matrix[i][j] > matrix[i][k] + matrix[k][j]) {
+                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    }
                 }
             }
         }
 
-
-
+        for (int i = 1; i <= v ; i++) {
+            min = Math.min(min, matrix[i][i]);
+        }
+        System.out.println(min == INF ? -1 : min);
     }
 
-    static class Edge implements Comparable<Edge>{
-        int to;
-        int weight;
-        Edge next;
-
-        public Edge(int to, int weight, Edge next) {
-            this.to = to;
-            this.weight = weight;
-            this.next = next;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(weight, o.weight);
-        }
-    }
 
 
 }
