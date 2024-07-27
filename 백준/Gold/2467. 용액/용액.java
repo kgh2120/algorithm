4438877e3a2b377108949bcb3c5f8d5411e7c6af
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-07-27 14:19:54 +0900

    [Gold V] Title: 용액, Time: 308 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,53 +1,112 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-
 
 public class Main {
 
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
 
-
-    static int[] arr;
-    static int min = Integer.MAX_VALUE;
-    static int minLeft;
-    static int minRight;
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> minus = new ArrayList<>();
+        List<Integer> plus = new ArrayList<>();
+
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int l = 0;
-        int r = n-1;
-        while (l < r) {
-            int left = arr[l];
-            int right = arr[r];
-            int abs = Math.abs(left+right);
-            if (min >= abs) {
-                minLeft = left;
-                minRight = right;
-                min = abs;
+            int water = Integer.parseInt(st.nextToken());
+            if (water < 0) {
+                minus.add(water);
+            } else {
+                plus.add(water);
             }
-
-            int absL = Math.abs(left);
-            int absR = Math.abs(right);
-            if(absL > absR){
-                l++;
-            }else
-                r--;
         }
-        sb.append(minLeft).append(" ")
-                .append(minRight);
-        System.out.println(sb);
+
+        int min = Integer.MAX_VALUE;
+
+        int left = minus.size() - 1;
+        int right = 0;
+
+        int ll = 0;
+        int rr = 0;
+
+        if (minus.size() >= 2) {
+            Integer l = minus.get(left);
+            Integer r = minus.get(left - 1);
+            int val = Math.abs(l + r);
+
+
+            if (min > val) {
+                ll = l;
+                rr = r;
+                min = val;
+            }
+        }
+
+        if (plus.size() >= 2) {
+            Integer l = plus.get(right);
+            Integer r = plus.get(right+1);
+            if (min > Math.abs(l + r)) {
+                ll = l;
+                rr = r;
+                min = Math.abs(l + r);
+            }
+        }
+
+
+        // 투포인터 하기
+
+
+        while (left >= 0 && right < plus.size()) {
+            Integer l = minus.get(left);
+            Integer r = plus.get(right);
+            int v = l + r;
+
+            if (min > Math.abs(v)) {
+                ll = l;
+                rr = r;
+                min = Math.abs(v);
+            }
+            if (v < 0) {
+                right++;
+            } else {
+                left--;
+            }
+        }
+
+        while (!plus.isEmpty() && left >= 0) {
+            Integer l = minus.get(left);
+            Integer r = plus.get(plus.size()-1);
+            int v = l + r;
+
+            if (min > Math.abs(v)) {
+                ll = l;
+                rr = r;
+                min = Math.abs(v);
+            }
+            left--;
+        }
+        while (!minus.isEmpty() && right < plus.size()) {
+            Integer l = minus.get(0);
+            Integer r = plus.get(right);
+            int v = l + r;
+
+            if (min > Math.abs(v)) {
+                ll = l;
+                rr = r;
+                min = Math.abs(v);
+            }
+            right++;
+        }
+
+
+
+        System.out.println(Math.min(ll, rr) + " " + Math.max(ll, rr));
+
 
     }
-
 
 
 }
