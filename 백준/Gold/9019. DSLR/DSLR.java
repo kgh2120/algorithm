e4c805250fcbd4453df29aba15327771375b35a0
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-07-31 11:58:04 +0900

    [Gold IV] Title: DSLR, Time: 5336 ms, Memory: 310204 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,144 +1,149 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Objects;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
-
+/*
+    @제약사항 : 6s, 256mb
+    @입력 범위 : 0 <= A,B < 10000
+    @문제 내용 :
+        4개의 명령을 수행하는 레지스터가 있음. 이 레지스터에 저장된 값을 4개의 명령을 수행하면서 A라는 값을 B로 바꾸는 최소한의 명령을 찾는 것이다.
+        4개의 명령은 아래와 같다.
+        D : if( n *=2 > 9999) n %= 10000
+        S : if(--n == 0 ) n = 9999
+        L : rotate left
+        R : rotate right
+    @주의 사항 :
+        0이 끼어있는 경우 문자를 숫자로 바꾸면 0이 사라지게 됨. 하지만 명령을 수행할 때엔 0의 존재를 잊으면 곤란함.
+    @예상 알고리즘 : BFS + 경로추적
+*/
 public class Main {
+
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static char[] commands = {'D', 'S', 'L', 'R'};
-    static boolean[] visited;
+    static int[][] visited;
+    static Queue<Node> q;
+    static int a;
+    static int b;
+    static int [] c = {0,1,2,3};
+    
 
     public static void main(String[] args) throws Exception {
-
-
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            visited = new boolean[10001];
-            Record record = (bfs(from, to)).record;
-            StringBuilder temp = new StringBuilder();
-            while (record != null) {
-                temp.append(record.command);
-                record = record.next;
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            visited = new int[10_000][2];
+            for (int j = 0; j < 10_000; j++) {
+                visited[j][0] = -1;
             }
-            sb.append(temp.reverse()).append("\n");
-        }
-        System.out.print(sb);
-    }
+            q = new ArrayDeque<>();
 
-    static Node bfs(int from, int to){
-        Queue<Node> q = new ArrayDeque<>();
-        Node start = new Node(from,null);
+            q.add(new Node(a));
+            visited[a][0] = a;
 
-        q.add(start);
-        while (!q.isEmpty()) {
-            Node poll = q.poll();
 
-            for (char command : commands) {
-                int action = action(command, poll.d1, poll.d2, poll.d3, poll.d4, poll.value);
-                if (!visited[action]) {
-                    visited[action] = true;
-                    Node node = new Node(action, new Record(command, poll.record));
-                    if (action == to) {
-                        return node;
+
+            loop: while(!q.isEmpty()) {
+                Node node = q.poll();
+                for (int cc : c) {
+
+                    Node next = node.command(cc);
+
+                    if(visited[next.value][0] != -1) continue;
+                    visited[next.value][0] = node.value;
+                    visited[next.value][1] = cc;
+                    q.add(next);
+                    if (next.value == b) {
+                        break loop;
                     }
-                    q.add(node);
                 }
+
             }
+
+            // b부터 찾아.
+            StringBuilder sub = new StringBuilder();
+            int cur = b;
+            while (cur != a) {
+                sub.append(convert(visited[cur][1]));
+                cur = visited[cur][0];
+            }
+            sb.append(sub.reverse()).append("\n");
         }
-        return null;
+        System.out.println(sb);
     }
 
-
-    static int action(char command, int d1, int d2, int d3, int d4, int value) {
-
-        switch (command) {
-            case 'D':
-                value = (value * 2 ) % 10000;
-                break;
-            case 'S':
-                if(value == 0)
-                    value = 9999;
-                else value--;
-                break;
-            case 'L':
-                value = d2*1000 + d3 * 100 + d4 * 10 + d1;
-                break;
-            case 'R':
-                // 마지막을 앞으로 보내기
-                value = d4*1000 + d1 * 100 + d2 * 10 + d3;
-                break;
+    static char convert(int v){
+        switch (v) {
+            case 0: return 'D';
+            case 1 : return 'S';
+            case 2 : return 'L';
+            case 3 :
+                return 'R';
+            default: return ' ';
         }
-        return value;
     }
 
-    // 자리수 구하기
-    private static int getN(int k){
+    static class Node{
 
-        int i = 0;
-        while ((k / 10) != 0) {
-            i++;
-            k = k/10;
-        }
-        return i;
-    }
-
-
-
-
-    static class Node {
+        int value;
         int d1;
         int d2;
         int d3;
         int d4;
-        int value;
-        Record record;
 
-        public Node(int value, Record record) {
-            this.d1 = value/1000;
-            this.d2 = value/100 % 10;
-            this.d3 = value/10 % 10;
-            this.d4 = value % 10;
+        public Node(int d1, int d2, int d3, int d4){
+            this.d1 = d1;
+            this.d2 = d2;
+            this.d3 = d3;
+            this.d4 = d4;
+            value = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
+        }
+
+        public Node(int value) {
             this.value = value;
-            this.record = record;
+            d1 = value / 1000;
+            d2 = (value % 1000) / 100;
+            d3 = (value % 100) / 10;
+            d4 = value % 10;
         }
 
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "d1=" + d1 +
-                    ", d2=" + d2 +
-                    ", d3=" + d3 +
-                    ", d4=" + d4 +
-                    ", value=" + value +
-                    ", record=" + record +
-                    '}';
+        public Node command(int command) {
+            switch (command) {
+                case 0 : return d();
+                case 1 : return s();
+                case 2 : return l();
+                case 3 : return r();
+                default: return null;
+            }
         }
+
+        public Node d(){
+            int temp = value * 2;
+            int v = temp > 9999 ? temp % 10000 : temp;
+            return new Node(v);
+        }
+
+        public Node s(){
+            int temp = value -1;
+            int v = value == 0 ? 9999 : temp;
+            return new Node(v);
+        }
+
+        public Node l(){
+            return new Node(d2,d3,d4,d1);
+        }
+        public Node r(){
+            return new Node(d4,d1,d2,d3);
+        }
+
     }
-
-    static class Record {
-
-        char command;
-        Record next;
-
-        public Record(char command, Record next) {
-            this.command = command;
-            this.next = next;
-        }
-
-
-    }
-
-
 
 }
