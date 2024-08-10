0d750ea6d53f419b84f1061bf81ce87e9b56c275
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-08-10 13:10:16 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 80 ms, Memory: 11936 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,44 +1,49 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
 
-    static int n;
-    static int[] dp;
-    static int[]arr;
-    static final int INIT = -1;
+
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder output = new StringBuilder();
+
 
     public static void main(String[] args) throws Exception {
-        n = Integer.parseInt(br.readLine());
-        dp = new int[n];
-        arr = new int[n];
-        Arrays.fill(dp,INIT);
-        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(input.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        st = new StringTokenizer(input.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int max = -1;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max,LIS(i));
-        }
-        System.out.println(max);
-//        System.out.println(Arrays.toString(dp));
-    }
-    private static int LIS(int index) {
-        if(dp[index] != INIT) return dp[index];
+        Arrays.fill(dp, 1);
 
-        dp[index] = 1;
-        for (int i = 0; i < index ; i++) {
-            if (arr[i] < arr[index]) {
-                dp[index] = Math.max(dp[index], LIS(i) +1);
+        for (int i = 0; i <n ; i++) {
+            int cur = arr[i];
+            for (int j = i; j >= 0 ; j--) {
+                int target = arr[j];
+
+                if (cur > target) {
+                    dp[i] = Math.max(dp[i], dp[j] +1);
+                }
             }
         }
-        return dp[index];
+
+        int max = -1;
+        for (int i : dp) {
+            max = Math.max(max, i);
+        }
+        System.out.println(max);
+
+
     }
+
+
 }
