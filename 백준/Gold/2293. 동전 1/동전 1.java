d78745131173f8dda03cacf960ecf736a424ff4f
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-08-14 12:31:19 +0900

    [Gold V] Title: 동전 1, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,43 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
- * - @author 규현
- * - @since 2024-03-03
- * - @limit memory : 4mb time : 0.5ms
- * - @performance
- * - @category #DP
- * - @note
- */
+
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+    static StringBuilder output = new StringBuilder();
+
+
+    static int[] dp;
+
+
+
     public static void main(String[] args) throws Exception {
 
-        st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(input.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[k + 1];
+        dp = new int[k + 1];
+        dp[0] = 1;
 
         for (int i = 0; i < n; i++) {
-            int number= Integer.parseInt(br.readLine());
-            if(number > k) continue;
-            arr[number]++;
-            for (int j = number; j <= k ; j++) {
-                arr[j] += arr[j-number];
+            int coin = Integer.parseInt(input.readLine());
+            for (int j = coin; j <= k; j++) {
+                dp[j] += dp[j - coin];
             }
         }
 
-
-        System.out.println(arr[k]);
+      
+        System.out.println(dp[k]);
 
 
     }
-
-
-
 
 }
