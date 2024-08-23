0bce09b3bbcd4ab7c6698eb7d6ffb1ec87e58d91
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-08-23 11:14:36 +0900

    [Silver II] Title: 나무 자르기, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -14,31 +14,31 @@ import java.io.*;
     
 */
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
+    static PScanner sc = new PScanner(System.in);
+
     static int n;
     static int m;
     static int[] trees;
     public static void main(String[] args) throws Exception {
         // 코드를 작성해주세요
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
+      
+        n = sc.nextInt();
+        m = sc.nextInt();
+
         trees = new int[n];
-        st = new StringTokenizer(br.readLine());
-        
+     
+
         int l = 0;
         int r = -1;
         for(int i = 0; i<n; i++){
-            trees[i] = Integer.parseInt(st.nextToken());
+            trees[i] = sc.nextInt();
             r = Math.max(r, trees[i]);
         }
         int ans = -1;
         while(l<=r){
             int mid = (l+r) /2;
             long height = cut(mid);
-            
+
             // 만약에 height가 m보다 적다면? mid를 낮춰야지
             if(height < m){
                 r = mid-1;
@@ -47,11 +47,11 @@ public class Main {
                 ans = Math.max(ans , mid);
             }
         }
-        
-        
+
+
         System.out.println(ans );
     }
-    
+
     static long cut(int h){
         long total = 0;
         for(int tree: trees){
@@ -59,4 +59,110 @@ public class Main {
         }
         return total;
     }
+    
+         public static class PScanner {
+                     private final InputStreamReader in;
+                     private final char[] buf;
+                     private final char[] cbuf;
+                     private int len, ptr;
+         
+                     public PScanner(InputStream input) {
+                         in = new InputStreamReader(input);
+                         buf = new char[8192];
+                         cbuf = new char[8192];
+                     }
+         
+                     public boolean hasNext() {
+                         consume();
+                         return ptr < len && buf[ptr] > ' ';
+                     }
+         
+                     public boolean hasNextInLine() {
+                         char c;
+                         while ((c = read()) <= ' ' && c != 0 && c != '\n') ;
+                         ptr--;
+                         return ptr < len && buf[ptr] > ' ';
+                     }
+         
+                     public String next() {
+                         consume();
+                         char[] cbuf = this.cbuf;
+                         int clen = 0;
+                         int start = ptr;
+                         while (true) {
+                             if (ptr < len) {
+                                 if (buf[ptr++] <= ' ') {
+                                     cbuf = copy(buf, start, cbuf, clen, ptr - start);
+                                     clen += ptr - start;
+                                     break;
+                                 }
+                             } else if (ptr == len) {
+                                 cbuf = copy(buf, start, cbuf, clen, ptr - start);
+                                 clen += ptr - start;
+                                 fill();
+                                 start = ptr;
+                             } else break;
+                         }
+                         ptr--;
+                         if (ptr < len) clen--;
+                         return new String(cbuf, 0, clen);
+                     }
+         
+                     private char[] copy(char[] src, int srcPos, char[] dest, int destPos, int length) {
+                         if (dest.length < destPos + length) dest = Arrays.copyOf(dest, dest.length << 1);
+                         System.arraycopy(src, srcPos, dest, destPos, length);
+                         return dest;
+                     }
+         
+                     public char nextChar() {
+                         consume();
+                         return read();
+                     }
+         
+                     public int nextInt() {
+                         consume();
+                         int v = 0;
+                         char c = read();
+                         boolean neg = c == '-';
+                         if (neg) c = read();
+                         do {
+                             v = v * 10 + c - '0';
+                         } while ('0' <= (c = read()) && c <= '9');
+                         ptr--;
+                         return neg ? -v : v;
+                     }
+         
+                     public long nextLong() {
+                         consume();
+                         long v = 0;
+                         char c = read();
+                         boolean neg = c == '-';
+                         if (neg) c = read();
+                         do {
+                             v = v * 10 + c - '0';
+                         } while ('0' <= (c = read()) && c <= '9');
+                         ptr--;
+                         return neg ? -v : v;
+                     }
+         
+                     private char read() {
+                         if (ptr == len) fill();
+                         return ptr < len ? buf[ptr++] : 0;
+                     }
+         
+                     private void fill() {
+                         try {
+                             len = in.read(buf);
+                             ptr = 0;
+                         } catch (IOException e) {
+                             throw new RuntimeException(e);
+                         }
+                     }
+         
+                     private void consume() {
+                         char c;
+                         while ((c = read()) <= ' ' && c != 0) ;
+                         ptr--;
+                     }
+                 }
 }
