6864426189335eab0906ecc86f234ac05f9a9425
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-08-28 09:37:46 +0900

    [Gold IV] Title: 최단경로, Time: 500 ms, Memory: 108876 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,80 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-/**
- * @author onyoo
- * @performance
- * @category
- * @note
- * @see https://www.acmicpc.net/problem/1753
- * @since 2023-10-13
- **/
+/*
+    얼핏보면 그냥 다익스트라같은데?
+    정점 a, b 사이에 여러 간선이 존재할 수 있다는 점은 고려해야 할 듯 싶다.
+    다익스트라 -> E log E 였나. 30만 * 20? 
+*/
 public class Main {
-    static class Node implements Comparable<Node>{
-        int idx;
-        int weight;
-
-        Node next;
-        public Node(int idx, int weight, Node next) {
-            this.idx = idx;
-            this.weight = weight;
-            this.next = next;
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    
+    static Edge [] graph;
+    static int n;
+    static int m;
+    static int startIndex;
+    static int[] dist;
+    
+    public static void main(String[] args) throws Exception {
+        // 코드를 작성해주세요
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        dist = new int[n+1];
+        Arrays.fill(dist, 10_0000_0000);
+        
+        startIndex = Integer.parseInt(br.readLine());
+        graph = new Edge[n+1];
+        for(int i = 0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            
+            graph[from] = new Edge(to,weight,graph[from]);
         }
-
-        @Override
-        public int compareTo(Node o) {
-            return weight-o.weight;
-        }
-    }
-    static int V,E,K;
-    static Node[] graph;
-    //인접 리스트
-    static int[] answer;
-    public static void main(String[] args) throws IOException {
-        PScanner sc = new PScanner(System.in);
-
-        V = sc.nextInt();
-        E = sc.nextInt();
-        K = sc.nextInt();
-        graph = new Node[V+1];
-        answer = new int[V + 1];
-        Arrays.fill(answer,Integer.MAX_VALUE);
-        for(int i=0;i<E;i++){
-
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            int weight = sc.nextInt();
-            graph[start] = new Node(end, weight, graph[start]);
-        }
-        // 단방향 그래프
-        answer[K] = 0;
-        bfs();
-        StringBuilder sb = new StringBuilder();
-        for(int i=1;i<V + 1;i++){
-            String info = answer[i] == Integer.MAX_VALUE ? "INF" : String.valueOf(answer[i]);
-            sb.append(info).append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    static void bfs(){
-        Queue<Node> que = new PriorityQueue<>();
-        que.add(new Node(K,0, null));
-        while(!que.isEmpty()){
-            Node curr = que.poll();
-            if(answer[curr.idx] < curr.weight) continue;
-            for(Node node=graph[curr.idx]; node != null; node = node.next){
-                if (answer[node.idx] > answer[curr.idx] + node.weight) {
-                    answer[node.idx] = answer[curr.idx] + node.weight;
-                    que.add(new Node(node.idx,node.weight + curr.weight, null));
+        
+        Queue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(startIndex,0,null));
+        dist[startIndex] = 0;
+        while(!pq.isEmpty()){
+            Edge e = pq.poll();
+            
+            if(dist[e.to] < e.weight) continue;
+            
+            for(Edge next = graph[e.to]; next != null; next = next.next){
+                if(dist[next.to] > next.weight + e.weight){
+                    dist[next.to] = next.weight + e.weight;
+                    pq.add(new Edge(next.to, dist[next.to], null));
                 }
             }
         }
+        
+        for(int i = 1; i<=n; i++){
+            String ans = dist[i] == 10_0000_0000 ? "INF" : Integer.toString(dist[i]);
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
     }
-    static class PScanner{private final InputStreamReader in;private final char[]buf;private int len,ptr;public PScanner(
-                InputStream input){in=new InputStreamReader(input);buf=new char[8192];}public boolean hasNext(){consume();return ptr<len&&buf[ptr]>' ';}public String next(){consume();char[]cbuf=new char[16];char clen=0;while((cbuf[clen++]=read())>' '){if(clen==cbuf.length)cbuf=Arrays.copyOf(cbuf,clen << 2);}return new String(cbuf,0,clen - 1);}public int nextInt(){consume();int v=0;char c=read();boolean neg=c=='-';if(neg)c=read();do{v=v * 10+c - '0';}while('0'<=(c=read())&&c<='9');return neg?-v:v;}public long nextLong(){consume();long v=0;char c=read();boolean neg=c=='-';if(neg)c=read();do{v=v * 10+c - '0';}while('0'<=(c=read())&&c<='9');return neg?-v:v;}private char read(){if(ptr==len)fill();return ptr<len?buf[ptr++]:0;}private void fill(){try{len=in.read(buf);ptr=0;}catch(
-                IOException e){throw new RuntimeException(e.getMessage());}}private void consume(){char c;while((c=read())<=' '&&c!=0);ptr--;}}
+    
+    static class Edge implements Comparable<Edge>{
+        int to;
+        int weight;
+        Edge next;
+        public Edge(int t, int w, Edge n ){
+            to = t;
+            weight = w;
+            next = n;
+        }
+        
+        @Override
+        public int compareTo(Edge o){
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
 }
