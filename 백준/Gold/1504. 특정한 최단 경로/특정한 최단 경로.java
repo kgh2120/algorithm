e5872b353b9d56defee42957762f74f1fab07bbe
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-09-02 15:50:42 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 1864 ms, Memory: 57828 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,106 +1,68 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
 
-    static List<List<Node>> adjList;
-
-    static class Node{
-        int f;
-        int t;
-        int w;
-
-        public Node(int f, int t, int w) {
-            this.f = f;
-            this.t = t;
-            this.w = w;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "f=" + f +
-                    ", t=" + t +
-                    ", w=" + w +
-                    '}';
-        }
-    }
-
-    static int[] dist;
-    static PriorityQueue<Node> pq;
-    static int n,v;
-
-    static final int MAX = 8_000_000;
-
+    static final int INF = 10_0000_0000;
     public static void main(String[] args) throws Exception {
+
         st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
-        dist = new int[n + 1];
-        adjList = new ArrayList<>(n);
-        for(int i = 0; i<=n; i++)
-            adjList.add(new ArrayList<>());
+        int[][] matrix = new int[n+1][n+1];
 
-        pq = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return Integer.compare(o1.w, o2.w);
-            }
-        });
 
-        for (int i = 0; i < v; i++) {
+
+        for (int i = 0; i<n+1; i++) {
+            Arrays.fill(matrix[i], INF);
+            matrix[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int f = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            adjList.get(f).add(new Node(f, t, w));
-            adjList.get(t).add(new Node(t,f, w));
+            matrix[f][t] = w;
+            matrix[t][f] = w;
         }
 
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        dijkstra(1);
-        int distA = dist[a];
-        int distB = dist[b];
-
-
-        dijkstra(a);
-        int distAToB = dist[b];
-        int distAToN = dist[n];
-        dijkstra(b);
-        int distBToA = dist[a];
-        int distBToN = dist[n];
-        int min = Math.min(distA + distAToB + distBToN, distB + distBToA + distAToN);
-        if(min >= MAX)
-            min = -1;
-        System.out.println(min);
-
-
-    }
-
-    private static void dijkstra(int startNode){
-        pq.clear();
-        Arrays.fill(dist, MAX);
-        dist[startNode] = 0;
-        pq.add(new Node(startNode, 0, 0));
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            if(dist[node.f] < node.w) continue;
-            for (Node n : adjList.get(node.f)) {
-                if (dist[n.t] > n.w + dist[n.f]) {
-                    dist[n.t] = n.w + dist[n.f];
-                    pq.add(new Node(n.t,0, dist[n.t]));
+        for(int k = 1; k<=n; k++){
+            for (int i = 1; i<= n; i++){
+                for (int j = 1; j <= n; j++) {
+                    if (matrix[i][j] > matrix[i][k] + matrix[k][j]) {
+                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    }
                 }
             }
         }
+
+        long ansA = matrix[1][a] + matrix[a][b] + matrix[b][n];
+        long ansB = matrix[1][b] + matrix[b][a] + matrix[a][n];
+
+        long ans = 0;
+        if(matrix[1][a] == INF || matrix[a][b] == INF || matrix[b][n] == INF)
+            ansA = -1;
+        if(matrix[1][b] == INF || matrix[b][a] == INF || matrix[a][n] == INF)
+            ansB = -1;
+
+        if(ansA != -1 && ansB != -1)
+            ans = Math.min(ansA,ansB);
+        else if(ansA == -1 && ansB != -1)
+            ans = ansB;
+        else ans = ansA;
+
+        System.out.println(ans);
+
+
     }
 
 }
