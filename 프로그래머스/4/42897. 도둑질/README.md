72db390033c1700ecd7986670137330f0d8f9c81
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-10-23 15:29:43 +0900

    [level 4] Title: 도둑질, Time: 21.17 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 19.65 ms
+메모리: 103 MB, 시간: 21.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 11:06:32
+2024년 10월 23일 15:29:40
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -4,17 +4,18 @@ class Solution {
     final int init = -1;
     public int solution(int[] money) {
         int answer = 0;
-        int[] dp1 = new int[money.length];
-        int[] dp2 = new int[money.length];
+     
+ 
 
         int length = money.length;
         if(length == 3){
             return Math.max(money[0], Math.max(money[1], money[2]));
         }
-        dp2[0] = money[0];
-        dp1[1] = money[1];
-        dp1[2] = money[2]; 
-        dp2[2] = money[0] + money[2];
+       int[][] dp = new int[2][money.length];
+        dp[1][0] = money[0];
+        dp[0][1] = money[1];
+        dp[0][2] = money[2]; 
+        dp[1][2] = money[0] + money[2];
         
             
         
@@ -22,18 +23,18 @@ class Solution {
             // 내 기준에서.. i-2 or i-3 어차피 i-1은 못먹음
             
             if(i == length-1){
-              dp1[i] = Math.max(dp1[i-2], dp1[i-3]) + money[i];
-              dp2[i] = Math.max(dp2[i-2], dp2[i-1]);
+              dp[0][i] = Math.max(dp[0][i-2], dp[0][i-3]) + money[i];
+              dp[1][i] = Math.max(dp[1][i-2], dp[1][i-1]);
                 continue;
             }
             
-            dp1[i] = Math.max(dp1[i-2], dp1[i-3]) + money[i];
-            dp2[i] = Math.max(dp2[i-2], dp2[i-3]) + money[i];
+            dp[0][i] = Math.max(dp[0][i-2], dp[0][i-3]) + money[i];
+            dp[1][i] = Math.max(dp[1][i-2], dp[1][i-3]) + money[i];
         }
         
         
         
-        answer = Math.max(dp1[length-1], dp2[length-1]);
+        answer = Math.max(dp[0][length-1], dp[1][length-1]);
         return answer;
     }
 
