dad012ea48a29661467ea8622dbd164fea634b8c
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-11-08 11:11:56 +0900

    [Gold IV] Title: 부분합, Time: 116 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -2,36 +2,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
+
+   static PScanner sc = new PScanner(System.in);
     static int[] arr;
     static int min = Integer.MAX_VALUE;
-    
-    
+
+
     public static void main(String[] args) throws Exception {
         // 코드를 작성해주세요
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
         
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
         arr = new int[n];
-        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-            
+            arr[i] = sc.nextInt();
+
         int l = 0;
         int r = 0;
         // r 커서에 닿는 거 먹고 ++ 하는게 맞을듯
         int acc = 0;
-    
+
         while(l<=r && r <= n){
             if(acc < k){ // 먹기
                 if(r == n) break;
                 acc += arr[r++];
                 continue;
             }
-            
+
             // 뱉기
             int length = r-l;
             min = Math.min(min,length);
@@ -41,4 +39,110 @@ public class Main {
             min = 0;
         System.out.println(min);
     }
+    
+         public static class PScanner {
+                     private final InputStreamReader in;
+                     private final char[] buf;
+                     private final char[] cbuf;
+                     private int len, ptr;
+         
+                     public PScanner(InputStream input) {
+                         in = new InputStreamReader(input);
+                         buf = new char[8192];
+                         cbuf = new char[8192];
+                     }
+         
+                     public boolean hasNext() {
+                         consume();
+                         return ptr < len && buf[ptr] > ' ';
+                     }
+         
+                     public boolean hasNextInLine() {
+                         char c;
+                         while ((c = read()) <= ' ' && c != 0 && c != '\n') ;
+                         ptr--;
+                         return ptr < len && buf[ptr] > ' ';
+                     }
+         
+                     public String next() {
+                         consume();
+                         char[] cbuf = this.cbuf;
+                         int clen = 0;
+                         int start = ptr;
+                         while (true) {
+                             if (ptr < len) {
+                                 if (buf[ptr++] <= ' ') {
+                                     cbuf = copy(buf, start, cbuf, clen, ptr - start);
+                                     clen += ptr - start;
+                                     break;
+                                 }
+                             } else if (ptr == len) {
+                                 cbuf = copy(buf, start, cbuf, clen, ptr - start);
+                                 clen += ptr - start;
+                                 fill();
+                                 start = ptr;
+                             } else break;
+                         }
+                         ptr--;
+                         if (ptr < len) clen--;
+                         return new String(cbuf, 0, clen);
+                     }
+         
+                     private char[] copy(char[] src, int srcPos, char[] dest, int destPos, int length) {
+                         if (dest.length < destPos + length) dest = Arrays.copyOf(dest, dest.length << 1);
+                         System.arraycopy(src, srcPos, dest, destPos, length);
+                         return dest;
+                     }
+         
+                     public char nextChar() {
+                         consume();
+                         return read();
+                     }
+         
+                     public int nextInt() {
+                         consume();
+                         int v = 0;
+                         char c = read();
+                         boolean neg = c == '-';
+                         if (neg) c = read();
+                         do {
+                             v = v * 10 + c - '0';
+                         } while ('0' <= (c = read()) && c <= '9');
+                         ptr--;
+                         return neg ? -v : v;
+                     }
+         
+                     public long nextLong() {
+                         consume();
+                         long v = 0;
+                         char c = read();
+                         boolean neg = c == '-';
+                         if (neg) c = read();
+                         do {
+                             v = v * 10 + c - '0';
+                         } while ('0' <= (c = read()) && c <= '9');
+                         ptr--;
+                         return neg ? -v : v;
+                     }
+         
+                     private char read() {
+                         if (ptr == len) fill();
+                         return ptr < len ? buf[ptr++] : 0;
+                     }
+         
+                     private void fill() {
+                         try {
+                             len = in.read(buf);
+                             ptr = 0;
+                         } catch (IOException e) {
+                             throw new RuntimeException(e);
+                         }
+                     }
+         
+                     private void consume() {
+                         char c;
+                         while ((c = read()) <= ' ' && c != 0) ;
+                         ptr--;
+                     }
+                 }
 }
