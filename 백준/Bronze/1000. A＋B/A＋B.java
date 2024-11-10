d39da7b5716ae0d445592d21b5ac05df7fb62b61
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-11-10 19:21:52 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,11 +1,9 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) ;
+    }
 }
