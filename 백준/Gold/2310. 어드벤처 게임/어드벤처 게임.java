d5557f5dd391b272335addfc61a18c7202b5b270
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-11-17 17:10:32 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 284 ms, Memory: 38992 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.java
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.java
@@ -1,126 +1,105 @@
-import java.io.*;
-import java.util.*;
+import com.sun.javafx.geom.Edge;
 
-/*
-240814
-4:16
-BJ2310 어드벤처 게임 - 골4
- */
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
 
-    static int n;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder answer = new StringBuilder();
 
-    static class Room {
-        char type;
+    public static void main(String[] args) throws Exception {
+
+
+        while(true){
+            int nOfRoom = Integer.parseInt(br.readLine());
+            if(nOfRoom == 0) break;
+            Room[] rooms = new Room[nOfRoom+1];
+//            int[] visited = new int[nOfRoom+1];
+//            Arrays.fill(visited,-1);
+            boolean[]visited = new boolean[nOfRoom+1];
+            for(int i = 1; i<=nOfRoom; i++){
+                st = new StringTokenizer(br.readLine());
+                String type = st.nextToken();
+                int cost = Integer.parseInt(st.nextToken());
+
+                rooms[i] = new Room(type, cost);
+                while(true){
+                    int toIndex = Integer.parseInt(st.nextToken());
+                    if(toIndex == 0) break;
+                    rooms[i].addTo(toIndex);
+                }
+            }
+            Queue<Current> q = new ArrayDeque<>();
+            q.add(new Current(1, 0));
+            String ans = "No";
+//            visited[1] = true;
+            // 만나면 해당 방부터 해결해라.
+            while(!q.isEmpty()){
+                Current cur = q.poll();
+
+                Room curRoom = rooms[cur.index];
+
+                int nextMoney = cur.money;
+                if (curRoom.type.equals("L")) {
+                    nextMoney = Math.max(cur.money, curRoom.value);
+                }
+                if (curRoom.type.equals("T") ) {
+                    if(cur.money < curRoom.value)
+                        continue;
+                    else {
+                        nextMoney = cur.money - curRoom.value;
+                    }
+                }
+                if (cur.index == nOfRoom) {
+                    ans = "Yes";
+                    break;
+                }
+                visited[cur.index] = true;
+                for (int next : curRoom.to) {
+//                    if (visited[next] < nextMoney) {
+//                        visited[next] = nextMoney;
+//                        q.add(new Current(next, nextMoney));
+//                    }
+                    if (!visited[next]) {
+                        q.add(new Current(next, nextMoney));
+                    }
+                }
+
+            }
+            answer.append(ans).append("\n");
+
+        }
+
+        System.out.println(answer);
+    }
+
+    static class Current{
+        int index;
         int money;
 
-        Room(char type, int money) {
-            this.type = type;
+        public Current(int index, int money) {
+            this.index = index;
             this.money = money;
         }
     }
 
-    static class Node {
-        int curRoom;
-        int leftMoney;
+    static class Room{
+        String type;
+        int value;
+        List<Integer> to;
 
-        Node(int curRoom, int leftMoney) {
-            this.curRoom = curRoom;
-            this.leftMoney = leftMoney;
+        public Room(String type, int value){
+            this.type = type;
+            this.value = value;
+            to = new ArrayList<>();
+        }
+
+        public void addTo(int index){
+            to.add(index);
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        while (true) {
-            n = Integer.parseInt(in.readLine());
-            if (n == 0) break;
-
-            List<List<Integer>> adjList = new ArrayList<>();
-            for (int i = 0; i <= n; ++i) {
-                adjList.add(new ArrayList<>());
-            }
-
-            Room[] rooms = new Room[n + 1];
-
-            for (int i = 1; i <= n; ++i) {
-                st = new StringTokenizer(in.readLine());
-
-                String type = st.nextToken();
-                if (type.equals("E")) {
-                    rooms[i] = new Room('E', Integer.parseInt(st.nextToken()));
-                } else if (type.equals("L")) {
-                    rooms[i] = new Room('L', Integer.parseInt(st.nextToken()));
-                } else if (type.equals("T")) {
-                    rooms[i] = new Room('T', Integer.parseInt(st.nextToken()));
-                }
-
-                while (true) {
-                    int to = Integer.parseInt(st.nextToken());
-                    if (to == 0) break;
-                    else adjList.get(i).add(to);
-                }
-            }
-
-            // bfs
-            boolean isPossible = false;
-
-            boolean[] visited = new boolean[n + 1];
-            Queue<Node> queue = new ArrayDeque<>();
-            queue.add(new Node(1, 0));
-
-            while (!queue.isEmpty()) {
-                Node cur = queue.poll();
-                int curRoom = cur.curRoom;
-                int leftMoney = cur.leftMoney;
-
-                // 현재 방 방문 가능 여부 확인
-                boolean check = true;
-
-                if (rooms[curRoom].type == 'E') {
-                    // 지금 방에 방문 가능.
-                } else if (rooms[curRoom].type == 'L') {
-                    if (rooms[curRoom].money > leftMoney) {
-                        leftMoney = rooms[curRoom].money;
-                    }
-                } else if (rooms[curRoom].type == 'T') {
-                    if (rooms[curRoom].money > leftMoney) {
-                        // 이 방으로 진입 불가. 여기서 끝
-                        check = false;
-                    } else {
-                        leftMoney -= rooms[curRoom].money; // 통행료 지불
-                    }
-                }
-
-                if (!check) continue; // 여기 방문 못 함...
-
-                // 방문 가능 -> 방문 처리 후 다음 방 확인
-                if (curRoom == n) { // n번 방까지 도착했다면...
-                    isPossible = true;
-                    break;
-                }
-
-                visited[curRoom] = true;
-                for (int next : adjList.get(cur.curRoom)) {
-                    if (!visited[next]) { // 아직 방문 안 한 곳이라면..
-                        // 방문
-                        queue.add(new Node(next, leftMoney));
-                    }
-                }
-            }
-
-            if (isPossible) {
-                sb.append("Yes\n");
-            } else {
-                sb.append("No\n");
-            }
-        }
-
-        System.out.println(sb);
-
-    }
 }
