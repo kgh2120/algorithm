83b77e11a8b01bc3971c8fe07dc81fa39eb7bdf2
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-11-17 16:08:07 +0900

    [Gold IV] Title: 해킹, Time: 756 ms, Memory: 158284 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.java
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -1,83 +1,73 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder answer = new StringBuilder();
-    
-    public static void main(String[] args) throws Exception {
-        // 코드를 작성해주세요
-        int tc = Integer.parseInt(br.readLine());
-        for(int t = 0; t <tc; t++){
-            st = new StringTokenizer(br.readLine());
-            int nOfPc = Integer.parseInt(st.nextToken());
-            int nOfDependencies = Integer.parseInt(st.nextToken());
-            int start = Integer.parseInt(st.nextToken());
-            
-            Edge [] graph = new Edge[nOfPc+1];
-            int[] dist = new int[nOfPc+1];
-            
-            for(int i = 0; i< nOfDependencies; i++){
-                st = new StringTokenizer(br.readLine());
-                
-                int to= Integer.parseInt(st.nextToken());
-                int from = Integer.parseInt(st.nextToken());
-                int cost = Integer.parseInt(st.nextToken());
-                
-                graph[from] = new Edge(to,cost,graph[from]);
-                
-                
-            }
-            
-            Arrays.fill(dist, 10_0000_0000);
-            dist[start] = 0;
-            
-            Queue<Edge> pq = new PriorityQueue<>();
-            
-            pq.add(new Edge(start,0,null));
-            
-            while(!pq.isEmpty()){
-                Edge current = pq.poll();
-                
-                if(dist[current.to] < current.cost) continue;
-                
-                for(Edge e = graph[current.to]; e != null; e = e.next){
-                    if(dist[e.to] > e.cost + dist[current.to]){
-                        dist[e.to] = e.cost + dist[current.to];
-                        pq.add(new Edge(e.to, e.cost + dist[current.to], null));
-                    }
-                }
-            }
-            
-            // 개수랑 숫자 세기
-            int max = -1;
-            int count = 0;
-            for(int i = 1; i<= nOfPc; i++){
-                if(dist[i] == 10_0000_0000) continue;
-                max = Math.max(max, dist[i]);
-                count++;
-            }
-            answer.append(count).append(" ").append(max).append("\n");
-        }
-        System.out.println(answer);
-    }
-    
-    static class Edge implements Comparable<Edge>{
-        int to;
-        int cost;
-        Edge next;
-        public Edge(int to, int cost, Edge next){
-            this.to = to;
-            this.cost= cost;
-            this.next = next;
-        }
-        
-        @Override
-        public int compareTo(Edge o){
-            return Integer.compare(this.cost, o.cost);
-        }
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder res = new StringBuilder();
+	static int INF = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws Exception {
+		int TC = readInt();
+		while (TC-- > 0) {
+			int n = readInt();
+			int d = readInt();
+			int c = readInt();
+			ArrayList<Node>[] graph = new ArrayList[n + 1]; 
+			for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+ 			for (int i = 0; i < d; i++) {
+				int n1 = readInt();
+				graph[readInt()].add(new Node(n1, readInt()));
+			}
+ 			
+ 			Queue<Node> q = new PriorityQueue<>();
+ 			q.add(new Node(c, 0));
+ 			
+ 			boolean[] visited = new boolean[n + 1];
+ 			
+ 			int[] list = new int[n + 1];
+ 			Arrays.fill(list, INF);
+ 			list[c] = 0;
+ 			
+ 			while(!q.isEmpty()) {
+ 				Node cur = q.poll();
+ 				visited[cur.n] = true;
+ 				if (cur.w > list[cur.n]) continue;
+ 				for (Node next : graph[cur.n]) {
+ 					if(list[next.n] < list[cur.n] + next.w) continue;
+ 					if(visited[next.n] && next.w == 0) continue;
+ 					list[next.n] = list[cur.n] + next.w;
+ 					q.add(new Node(next.n, list[next.n]));
+ 				}
+ 			}
+ 			int cnt = 0;
+ 			int time = 0;
+ 			for (int t : list) {
+ 				if (t == INF) continue;
+ 				cnt++;
+ 				time = Math.max(time, t);
+ 			}
+ 			res.append(cnt).append(" ").append(time).append("\n");
+ 		}
+		System.out.println(res);
+ 	}
+	
+	static class Node implements Comparable<Node> {
+		int n, w;
+	
+		Node(int n, int w) {
+			this.n = n;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(Main.Node o) {
+			return this.w - o.w;
+		}
+	}
+
+	static int readInt() throws Exception {
+		if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+		return Integer.parseInt(st.nextToken());
+	}
 }
