9f0baec2aaa160c846ea14216474be45c78910be
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-11-26 14:35:09 +0900

    [Platinum V] Title: 이진 탐색 트리, Time: 732 ms, Memory: 88568 KB -BaekjoonHub

--- 백준/Platinum/2957. 이진 탐색 트리/이진 탐색 트리.java
+++ 백준/Platinum/2957. 이진 탐색 트리/이진 탐색 트리.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PScanner sc = new PScanner(System.in);
     static StringBuilder answer = new StringBuilder();
 
 
@@ -13,9 +13,9 @@ public class Main {
 
         TreeMap<Integer, Node> map = new TreeMap<>();
 
-        int n = Integer.parseInt(br.readLine());
+        int n = sc.nextInt();
 
-        int root = Integer.parseInt(br.readLine());
+        int root = sc.nextInt();
 
         map.put(root, new Node(0));
         long c = 0;
@@ -23,7 +23,7 @@ public class Main {
         for (int i = 1; i < n; i++) {
 
 
-            int k = Integer.parseInt(br.readLine());
+            int k = sc.nextInt();
 
             Map.Entry<Integer, Node> lowerEntry = map.lowerEntry(k);
             Map.Entry<Integer, Node> higherEntry = map.higherEntry(k);
@@ -77,4 +77,6 @@ public class Main {
             this.depth = depth;
         }
     }
+        // @formatter:off
+            static class PScanner{private final InputStreamReader in;private final char[]buf;private final char[]cbuf;private int len,ptr;public PScanner(InputStream input){in=new InputStreamReader(input);buf=new char[8192];cbuf=new char[8192];}public boolean hasNext(){consume();return ptr<len&&buf[ptr]>' ';}public boolean hasNextInLine(){char c;while((c=read())<=' '&&c!=0&&c!='\n');ptr--;return ptr<len&&buf[ptr]>' ';}public String next(){consume();char[]cbuf=this.cbuf;int clen=0;int start=ptr;while(true){if(ptr<len){if(buf[ptr++]<=' '){cbuf=copy(buf,start,cbuf,clen,ptr - start);clen+=ptr - start;break;}}else if(ptr==len){cbuf=copy(buf,start,cbuf,clen,ptr - start);clen+=ptr - start;fill();start=ptr;}else break;}ptr--;if(ptr<len)clen--;return new String(cbuf,0,clen);}private char[]copy(char[]src,int srcPos,char[]dest,int destPos,int length){if(dest.length<destPos+length)dest=Arrays.copyOf(dest,dest.length << 1);System.arraycopy(src,srcPos,dest,destPos,length);return dest;}public char nextChar(){consume();return read();}public int nextInt(){consume();int v=0;char c=read();boolean neg=c=='-';if(neg)c=read();do{v=v * 10+c - '0';}while('0'<=(c=read())&&c<='9');ptr--;return neg?-v:v;}public long nextLong(){consume();long v=0;char c=read();boolean neg=c=='-';if(neg)c=read();do{v=v * 10+c - '0';}while('0'<=(c=read())&&c<='9');ptr--;return neg?-v:v;}private char read(){if(ptr==len)fill();return ptr<len?buf[ptr++]:0;}private void fill(){try{len=in.read(buf);ptr=0;}catch(IOException e){throw new RuntimeException(e);}}private void consume(){char c;while((c=read())<=' '&&c!=0);ptr--;}}
 }
