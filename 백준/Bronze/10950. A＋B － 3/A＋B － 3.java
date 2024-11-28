2c18cd95a3774993398450aa282fddef6ebf808c
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-11-28 21:26:07 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 13076 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 코드를 작성해주세요
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while(n-->0){
+            System.out.println(sc.nextInt() + sc.nextInt());
+        }
+    }
+}
