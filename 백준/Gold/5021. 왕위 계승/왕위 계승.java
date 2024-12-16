74f023b1e7e1e2102975cbee4ba6e3ec46ee5d5d
김규현 <76154390+kgh2120@users.noreply.github.com>
2024-12-16 09:46:54 +0900

    [Gold IV] Title: 왕위 계승, Time: 64 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Gold/5021. 왕위 계승/왕위 계승.java
+++ 백준/Gold/5021. 왕위 계승/왕위 계승.java
@@ -1,75 +1,108 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static Map<String, People> parentMap;
     static String root;
-    static Map<String, Parent> jokbo;
-    static Map<String, Double> jokboDp;
+
+    static long rootRank;
+
+
 
     public static void main(String[] args) throws Exception {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
+
+        parentMap = new HashMap<>();
         root = br.readLine();
 
-        jokbo = new HashMap<>();
-        jokboDp = new HashMap<>();
-        jokbo.put(root, null);
-        jokboDp.put(root, 1.0);
+        rootRank = (long) Math.pow(2, n);
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            jokbo.put(st.nextToken(), new Parent(st.nextToken(), st.nextToken()));
+            String child = st.nextToken();
+
+            String parentLeft  = st.nextToken();
+            String parentRight = st.nextToken();
+
+            parentMap.put(child, new People(parentLeft, parentRight, -1));
         }
 
-        double min = -1 ;
-        String answer = null;
+
+        long maxRank = Long.MIN_VALUE;
+        String ranker = null;
         for (int i = 0; i < m; i++) {
-            String hubo = br.readLine();
-            double rank = find(hubo);
-            if (rank > min) {
-                min = rank;
-                answer = hubo;
+
+            String name = br.readLine();
+            long rank = calculateRank(name);
+            if(rank < 1)
+                continue;
+
+            
+
+            if (maxRank < rank) {
+                maxRank = rank;
+                ranker = name;
             }
+
         }
-        System.out.println(answer);
+        System.out.println(ranker);
 
-
+        
     }
 
-    private static double find(String name) {
-        // 왼쪽 / 2 + 오른쪽 /2 할꺼야.
-        // name에서 dp에 있다면 즉시 리턴해
-        // !contains 면 0을 리턴해.
-        // null을 가지고 있다면 1을 리턴해.
+    static long calculateRank(String name){
 
-        if(jokboDp.containsKey(name))
-            return jokboDp.get(name);
-        if(!jokbo.containsKey(name))
-            return 0;
-        Parent parent = jokbo.get(name);
-        if(parent == null)
-            return 1;
+        // 일단 이름이 있는지 체크
+        // 이름이 없는데 root라면 1, root가 아니라면 0
 
-        double rank = find(parent.left) / 2 + find(parent.right) / 2;
-        jokboDp.put(name, rank);
-        return rank;
+        People people = parentMap.get(name);
+        if(people == null){
+            return root.equals(name) ? rootRank : 0;
+        }
+
+        // 이름이 있다면 재귀를 타면서 계산하기
+
+        if(people.rank != -1)
+            return people.rank;
+
+
+        long sum = 0;
+        sum += calculateRank(people.leftName);
+        sum += calculateRank(people.rightName);
+
+        people.rank = sum / 2;
+
+        return people.rank;
     }
 
-    static class Parent {
-        String left;
-        String right;
 
-        public Parent(String left, String right) {
-            this.left = left;
-            this.right = right;
+    static class People {
+        String leftName;
+        String rightName;
+        long rank;
+
+        // people이 없으면 rank가 0으로 취급하면 될듯.
+        public People(String leftName, String rightName, long rank) {
+            this.leftName = leftName;
+            this.rightName = rightName;
+            this.rank = rank;
+        }
+
+        @Override
+        public String toString() {
+            return "People{" +
+                    "leftName='" + leftName + '\'' +
+                    ", rightName='" + rightName + '\'' +
+                    ", rank=" + rank +
+                    '}';
         }
     }
-
 }
