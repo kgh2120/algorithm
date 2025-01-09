514844c9a2322f58329b0c3c68193f0e21c27449
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-01-09 10:46:09 +0900

    [Gold IV] Title: ⚾, Time: 496 ms, Memory: 60968 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -1,114 +1,113 @@
 import java.util.*;
 import java.io.*;
 
-
-/*
-    author : 규현
-    date : 2023-08-23
-    url : 
-    performance :
-    category : 
-    note:
+/**
 
 
 
-*/
-class Main {
+ **/
+public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static int[][] hitterResult;
-    static int max = Integer.MIN_VALUE;
-    static int n;
-    static boolean[] s;
-    static int currentScore;
-    static int outCount;
-    static int currentRunner;
 
+
+    static int [] order;
+    static boolean[] selected = new boolean[9];
+    static int maxIning;
+    static int[][] result;
+
+    static int maxScore;
+    static int cursor = -1;
     public static void main(String[] args) throws Exception {
-        n = Integer.parseInt(br.readLine());
-        hitterResult = new int[n][10];
-        s = new boolean[10];
+        // 코드를 작성해주세요
+        maxIning = Integer.parseInt(input.readLine());
+        result = new int[maxIning][9];
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= 9; j++) {
-                hitterResult[i][j] = Integer.parseInt(st.nextToken());
+        order = new int[9];
+        selected[0] = true;
+
+        for (int i = 0; i < maxIning; i++) {
+            st = new StringTokenizer(input.readLine());
+            for (int j = 0; j < 9; j++) {
+                result[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        permutation(0, new int[9]);
-        System.out.println(max);
-        ;
+        play(0);
+
+        System.out.println(maxScore);
+
     }
 
-    private static void permutation(int depth, int[] selected) {
+    static void play(int depth){
+
         if (depth == 9) {
-            playBaseball(selected);
+            baseball();
             return;
         }
+
         if (depth == 3) {
-            selected[3] = 1;
-            permutation(depth + 1, selected);
+            play(depth+1);
             return;
         }
-        for (int i = 2; i <= 9; i++) {
-            if (s[i]) continue;
-            s[i] = true;
-            selected[depth] = i;
-            permutation(depth + 1, selected);
-            s[i] = false;
+
+        for (int i = 1; i < 9; i++) {
+            if(selected[i]) continue;
+            order[depth] = i;
+            selected[i] = true;
+            play(depth+1);
+            selected[i] = false;
         }
     }
 
-    private static void playBaseball(int[] selected) {
-        // 게임 진행
-        // 각 이닝당 Queue에서 선수를  꺼내서 타격을 한다.
-        // 그 결과에 따라서 runnerQueue에 반영을 한다.
-        // outCount가 3이 되면 이닝을 넘긴다.
-        currentScore = 0;
+    static void baseball(){
+        int score = 0;
+        cursor = -1;
+        int ining = 0;
 
-        int inning = 0;
-        int no = 0;
-        while (inning < n) {
-            outCount = 0;
-            currentRunner = 1;
-            // hit
+        while (ining < maxIning) {
+            boolean[] field = new boolean[4];
+            field[0] = true;
+            int outCount = 0;
             while (outCount < 3) {
-                if (no > 8)
-                    no = 0;
-                hit(hitterResult[inning][selected[no++]]);
-            }
 
-            inning++;
-        }
-        max = Math.max(max, currentScore);
-    }
+                int playerNumber = getPlayer();
+                int r = result[ining][playerNumber];
 
-    private static void hit(int hit) {
-
-        if (hit == 0) {
-            outCount++;
-            return;
-        }
-        //
-
-        currentRunner = currentRunner << hit; // 먼저 밀고
-        currentRunner = currentRunner | 1 << hit; // 해당 위치에 넣어주기
-        while (currentRunner >= 16) {
-            for (int i = 4; i <= 7 ; i++) {
-                if ((currentRunner & 1 << i) > 0) {
-                    currentScore++;
-                    currentRunner = currentRunner & ~ (1 << i);
+                if (r == 0) {
+                    outCount++;
+                } else {
+                    for (int i = 3; i >= 0; i--) {
+                        if(!field[i]) continue;
+                        if(i != 0)
+                            field[i] = false;
+                        if (i + r > 3) {
+                            score++;
+                        } else {
+                            field[i+r] = true;
+                        }
+                    }
                 }
             }
-
+            ining++;
         }
-
-
-
+        if (maxScore < score) {
+            maxScore = score;
+//            System.out.println(Arrays.toString(order));
+        }
+//        maxScore = Math.max(maxIning, score);
     }
+
+
+
+
+    static int getPlayer(){
+        if(++cursor == 9)
+            cursor = 0;
+        return order[cursor];
+    }
+
 
 
 }
