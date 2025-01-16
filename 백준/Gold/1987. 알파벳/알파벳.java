3bf0f460d0ee85d8ade26591c24914a859fede06
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-01-16 15:50:53 +0900

    [Gold IV] Title: 알파벳, Time: 148 ms, Memory: 12552 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,62 +1,68 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    
+
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    
+
     static int maxRow;
     static int maxCol;
-    
-    static int [][] deltas = {{-1,0}, {0,-1}, {1,0}, {0,1}};
-    
-    static char[][] map;    
-    // 26
-    
 
-    
+    static int [][] deltas = {{-1,0}, {0,-1}, {1,0}, {0,1}};
+
+    static int[][] map;
+    // 26
+
+    static int max = 0;
+
+    static int[][] visited;
+
+
     public static void main(String[] args) throws Exception{
         // 코드를 작성해주세요
-        
+
         st = new StringTokenizer(input.readLine());
         maxRow = Integer.parseInt(st.nextToken());
         maxCol = Integer.parseInt(st.nextToken());
-        
-        map = new char[maxRow][];
-        
+
+        map = new int[maxRow][maxCol];
+        visited = new int[maxRow][maxCol];
         for(int i = 0; i<maxRow; i++){
-            map[i] = input.readLine().toCharArray();
+            String line = input.readLine();
+            for(int j = 0; j<maxCol; j++)
+                map[i][j] = line.charAt(j) - 'A';
         }
-        
-        boolean[] selected = new boolean[26];
-        selected[map[0][0] - 'A'] = true;
-        int answer = dfs(0,0, selected);
-        System.out.println(answer);
+
+
+        dfs(0,0, 1, 1 << map[0][0]);
+        System.out.println(max);
 
     }
-    
-    static int dfs(int row, int col, boolean[] selected){
-        
-        int max = 0;
-        // 4방향 탐색
+
+    static void dfs(int row, int col, int score, int selectedBit ){
+        // 해당 부분에 똑같은 조건으로 온 적 있거나 이미 모든 알파벳을 모은 경우는 pass
+        if(visited[row][col] == selectedBit ||  max == 26){
+            return;
+        }
+
+        visited[row][col] = selectedBit;
+
+
         for(int [] delta : deltas){
             int nr = row + delta[0];
             int nc = col + delta[1];
-            
-            if(isIn(nr,nc)){
-                char next = map[nr][nc];
-                if(!selected[next-'A']){
-                    selected[next-'A'] = true;
-                    max = Math.max(max, dfs(nr,nc, selected));
-                    selected[next-'A'] = false;
-                    
-                }
+            // 범위 내에 있는지, 방문한 적 있는지(bit로 체크)
+            if(isIn(nr,nc) && (selectedBit & (1 << map[nr][nc])) == 0){
+                dfs(nr,nc, score+1, selectedBit | (1 << map[nr][nc]));
+
             }
         }
-        return 1 + max;
+
+        max = Math.max(max,score);
     }
-    
+
     static boolean isIn(int row, int col){
         return row >= 0 && row < maxRow && col >= 0 && col < maxCol;
     }
 }
+
