0014ad76ba97ea5dd5d61a70f9fd71950407ccd0
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-01-17 14:21:58 +0900

    [Gold I] Title: 로봇 청소기, Time: 416 ms, Memory: 60780 KB -BaekjoonHub

--- 백준/Gold/4991. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/4991. 로봇 청소기/로봇 청소기.java
@@ -1,161 +1,145 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.*;
-
-
-
-/*
-    로봇이 상하좌우로 이동하면서 더러운 칸을 깨끗하게 만들어준다.
-    로봇은 같은 곳을 다시 갈 수 있음.
-    모든 더러운 곳을 처리하는 최소 시간을 출력하고, 그렇지 못하는 경우엔 -1을 출력하라고 함.
-    같은 곳을 다시 갈 수 있다고 해도, 무의미한 상태로 다시 가는건 의미가 없다.
-
-    visited를 체크할 때, 어떤 더러운 칸을 내가 처리했는지에 대해서 알고있으면 될 듯 싶다.
-
-    그럼 visited 배열을 bitmasking을 해가지고 만들면 될 듯 싶음.
-
-    그리고 visited가 다 찼을 경우, bit+1이 1 << 더러운 칸과 같을 경우에 time을 출력하면 될 듯 싶다.
-
-
-    필요한 것, 먼지 클래스,
-    상하좌우 델타 배열,
-    3차원 visited 배열,
-    청소기 위치.
-    BFS를 돌 큐
-
- */
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
 
-    static int n,m;
-    static char[][] matrix;
-    static int sr, sc;
-    static int[][] deltas = {{-1,0},{1,0},{0,-1},{0,1}};
+    static int maxRowSize;
+    static int maxColSize;
 
-    static final char BLOCK = 'x', DIRT = '*', NORMAL = '.', ROBOT = 'o';
+    static int[][]deltas = new int[][]{
+            {-1,0}, {0,-1},{0,1},{1,0}
+    };
 
-    static List<Dirt> dirtList;
-    static boolean[][][] visited;
-    public static void main(String[] args) throws Exception {
+    static final char BLOCK = 'x';
+
+    public static void main(String[] args) throws Exception{
+        StringBuilder answer = new StringBuilder();
+
         while (true) {
-            st = new StringTokenizer(br.readLine());
-            m = Integer.parseInt(st.nextToken());
-            n = Integer.parseInt(st.nextToken());
-            if(n == 0 && m == 0) break;
-            matrix = new char[n][m];
-            dirtList = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                String line = br.readLine();
-                for (int j = 0; j < m; j++) {
-                    char com = line.charAt(j);
-                    matrix[i][j] = com;
-                    if (com == ROBOT) {
-                        sr = i;
-                        sc = j;
-                        matrix[i][j] = NORMAL;
-                    } else if (com == DIRT) {
-                        dirtList.add(new Dirt(i, j));
+            st = new StringTokenizer(input.readLine());
+
+            maxColSize = Integer.parseInt(st.nextToken());
+             maxRowSize = Integer.parseInt(st.nextToken());
+
+
+            if(maxRowSize == 0 && maxColSize == 0)
+                break;
+
+
+            int sR=0;
+            int sC=0;
+
+            char[][] map = new char[maxRowSize][maxColSize];
+
+            char dirt = 'a';
+
+            for (int i = 0; i < maxRowSize; i++) {
+                String line  = input.readLine();
+                for (int j = 0; j < maxColSize; j++) {
+                    char c = line.charAt(j);
+                    if (c == 'o') {
+                        sR=i;
+                        sC=j;
+                        c='.';
                     }
+
+                    if (c == '*') {
+                        c = ++dirt;
+                    }
+                    map[i][j] = c;
                 }
             }
-            if (dirtList.isEmpty()) {
-                sb.append("0\n");
-            } else {
-                visited = new boolean[1 << dirtList.size()][n][m];
-                sb.append(bfs()).append("\n");
+
+
+            int numberOfDirt =  dirt - 'a';
+            boolean[][][]visited = new boolean[1 << (numberOfDirt+1)][maxRowSize][maxColSize];
+
+            Queue<Robot> q = new ArrayDeque<>();
+            q.add(new Robot(numberOfDirt, 1, sR, sC));
+            visited[1][sR][sC] = true;
+
+            if (numberOfDirt == 0) {
+                answer.append(0).append("\n");
+                continue;
             }
-        }
-        System.out.print(sb);
 
 
-    }
+            int turn = 0;
+            boolean find = false;
+            loop: while (!q.isEmpty()) {
+                turn++;
+                int size = q.size();
+                while (size-- > 0) {
 
-    private static int bfs(){
-        Queue<Pos> q = new ArrayDeque<>();
-        visited[0][sr][sc] = true;
-        q.add(new Pos(sr,sc,0));
-        int time = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            time++;
-            while (size-- > 0) {
-                Pos poll = q.poll();
-                int cnt = poll.count;
-                for (int[] delta : deltas) {
-                    int nr = poll.row + delta[0];
-                    int nc = poll.col + delta[1];
-                    if (isIn(nr, nc) && !visited[cnt][nr][nc]) {
-                        if (matrix[nr][nc] == NORMAL) {
-                            visited[cnt][nr][nc] = true;
-                            q.add(new Pos(nr,nc,cnt));
-                        } else if (matrix[nr][nc] == DIRT) {
-//                            visited[cnt][nr][nc] = true;
-                            int index = dirtList.indexOf(new Dirt(nr, nc));
-                            if (!visited[cnt | (1 << index)][nr][nc]) {
-                                visited[cnt | (1 << index)][nr][nc] = true;
-                                q.add(new Pos(nr,nc, cnt | (1 << index)));
-                            }
-                            if ((cnt | (1 << index))+ 1 == 1 << dirtList.size()) {
-                                return time;
+                    Robot robot = q.poll();
+
+                    for (int[] delta : deltas) {
+                        int nr = robot.row + delta[0];
+                        int nc = robot.col + delta[1];
+                        if (isIn(nr, nc) && map[nr][nc] != BLOCK && !visited[robot.dimension][nr][nc]) {
+                            visited[robot.dimension][nr][nc] = true;
+                            if (Character.isAlphabetic(map[nr][nc])) {
+                                int nD = map[nr][nc] - 'a';
+                                if ((robot.dimension & (1 << nD)) == 0) {
+                                    Robot nextRobot = new Robot(robot.numberOfDirt - 1,robot.dimension | (1 << nD),  nr, nc);
+                                    q.add(nextRobot);
+                                    visited[nextRobot.dimension][nr][nc] = true;
+                                    if (nextRobot.numberOfDirt == 0) {
+                                        find = true;
+                                        break loop;
+                                    }
+                                }
+                                q.add(new Robot(robot.numberOfDirt,robot.dimension,  nr, nc));
+                            }else {
+                                q.add(new Robot(robot.numberOfDirt,robot.dimension,  nr, nc));
                             }
                         }
+
                     }
+
                 }
+
             }
+
+            answer.append(find ? turn : -1).append("\n");
         }
-        return -1;
+        System.out.println(answer);
+
+
+
+
     }
 
-    private static boolean isIn(int row, int col) {
-        return row >= 0 && row < n && col >= 0 && col < m && matrix[row][col] != BLOCK;
+
+
+    static boolean isIn(int row, int col) {
+        return row>= 0 && row < maxRowSize && col >= 0 && col < maxColSize;
     }
 
-    static class Dirt{
+    static class Robot{
+        int numberOfDirt;
+        int dimension;
         int row;
         int col;
 
-        public Dirt(int row, int col) {
+        public Robot(int numberOfDirt, int dimension, int row, int col) {
+            this.numberOfDirt = numberOfDirt;
+            this.dimension = dimension;
             this.row = row;
             this.col = col;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Dirt dirt = (Dirt) o;
-            return row == dirt.row && col == dirt.col;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(row, col);
-        }
-    }
-
-    static class Pos{
-        int row;
-        int col;
-        int count;
-
-        public Pos(int row, int col, int count) {
-            this.row = row;
-            this.col = col;
-            this.count = count;
         }
 
         @Override
         public String toString() {
-            return "Pos{" +
-                    "row=" + row +
+            return "Robot{" +
+                    "numberOfDirt=" + numberOfDirt +
+                    ", dimension=" + Integer.toBinaryString(dimension) +
+                    ", row=" + row +
                     ", col=" + col +
-                    ", count=" + count +
                     '}';
         }
     }
