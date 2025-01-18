6274d08acc2f7137a2db5090978a24321078d119
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-01-18 12:55:06 +0900

    [Gold V] Title: 트리, Time: 64 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,90 +1,77 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
- class Main {
+/*
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    2번째 줄에서 주어진 parent에 대한 정보를 토대로
+    Tree를 구성할 수 있을 것임. 
+    그러면 루트부터 타고 내려가면서 체크가 가능함.
+    
+    트리를 만들고, 제거하기로 선택한 애한테 접근을 못하게 하면 크게 어렵지 않게 풀 수 있을듯?
+
+*/
+
+public class Main {
+    
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-
-
-    static int nOfNode;
-    static int root;
-    static int deletedNodeIndex;
-    static Node[] children;
+    
+    
+    
     public static void main(String[] args) throws Exception {
-        setVariables();
-
-        // delete Node
-        deleteNode();
-
-        // find LeafNode
-        System.out.println(findLeafNodes());
-
+        // 코드를 작성해주세요
+        
+        int n = Integer.parseInt(input.readLine());
+        
+        st = new StringTokenizer(input.readLine());
+        
+        
+        Node[] tree = new Node[n];
+        int rootIndex = 0;
+        for(int i = 0; i<n; i++){
+            int v = Integer.parseInt(st.nextToken());
+            
+            if(v == -1){
+                rootIndex = i;
+            } else {
+                tree[v] = new Node(i, tree[v]);
+            }
+        }
+        
+        
+        int deletedTree = Integer.parseInt(input.readLine());
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(rootIndex);
+        
+        int leafNodeCount = 0;
+        
+        while(!q.isEmpty() && rootIndex != deletedTree ){
+            int treeIndex = q.poll();
+            
+            int count = 0;
+            
+            for(Node node = tree[treeIndex]; node  != null; node  = node .next){
+                if(node.v == deletedTree) continue;
+                q.add(node.v);
+                count++;
+            }
+            
+            if(count == 0)
+                leafNodeCount++;
+        }
+        
+        System.out.println(leafNodeCount);
+        
     }
-
-     private static int findLeafNodes() {
-        int nOfLeafNodes = 0;
-         Queue<Integer> q = new ArrayDeque<>();
-         q.add(root);
-         while (!q.isEmpty()) {
-             Integer nodeNumber = q.poll();
-             if (nodeNumber == deletedNodeIndex) {
-                 continue;
-             }
-
-             if (children[nodeNumber] == null) {
-                 nOfLeafNodes++;
-             } else {
-                 boolean isLeaf = true;
-                 for (Node next = children[nodeNumber]; next != null; next = next.next) {
-                     if(next.num == deletedNodeIndex)
-                         continue;
-                     q.add(next.num);
-                     isLeaf = false;
-                 }
-                 if (isLeaf) {
-                     nOfLeafNodes++;
-                 }
-             }
-         }
-         return nOfLeafNodes;
-     }
-
-     private static void deleteNode() {
-         children[deletedNodeIndex] = null;
-     }
-
-     private static void setVariables() throws IOException {
-         nOfNode = Integer.parseInt(br.readLine());
-         st = new StringTokenizer(br.readLine());
-         children = new Node[nOfNode];
-         for (int i = 0; i < nOfNode; i++) {
-             // i => number,
-             int parent = Integer.parseInt(st.nextToken());
-
-             if (parent == -1) {
-                 root = i;
-                 continue;
-             }
-             children[parent] = new Node(children[parent], i);
-         }
-
-         deletedNodeIndex = Integer.parseInt(br.readLine());
-     }
-
-
-     static class Node{
+    
+    static class Node{
+        int v;
         Node next;
-        int num;
-        public Node(Node next, int num) {
-            this.next = next;
-            this.num = num;
+        
+        public Node(int v, Node n){
+            this.v = v;
+            this.next = n;
         }
     }
-
-
-
-
 }
