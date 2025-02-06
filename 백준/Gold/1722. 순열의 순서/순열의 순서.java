3cbee1b2742fb15e4d26916e9e04b2222021e81e
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-02-06 12:26:27 +0900

    [Gold V] Title: 순열의 순서, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,89 +1,73 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
+public class Main {
 
-class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
+    static int n;
 
     static long[] factorial;
+    static List<Integer> list;
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        StringBuilder sb = new StringBuilder();
 
-    static List<Integer> number;
-    static StringBuilder sb = new StringBuilder();
-
-    static int n;
-    static int type;
-    static long problem;
-    static int[] target;
-    static long answer;
-    public static void main(String[] args) throws Exception {
-
-
-        n = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        type = Integer.parseInt(st.nextToken());
-
-        if (type == 1) {
-            problem = Long.parseLong(st.nextToken());
-        }else{
-            target = new int[n];
-            for (int i = 0; i < n; i++) {
-                target[i] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-
+        n = Integer.parseInt(input.readLine());
         factorial = new long[n+1];
-
-        factorial[0] = 1;
-        for (int i = 1; i <= n ; i++) {
-            factorial[i] = factorial[i-1] * i;
+        factorial[0] = 1L;
+        factorial[1] = 1L;
+        for(int i = 2; i<=n; i++){
+            factorial[i] =  (factorial[i-1] * i);
         }
 
-        number = new ArrayList<>(20);
-        for (int i = 1; i <= n ; i++) {
-            number.add(i);
-        }
+        list = new ArrayList<>();
+        for(int i = 1; i <= n; i++)
+            list.add(i);
 
-        if (type == 1) {
-            problem--;
-            problem1(1);
-        }else{
-            problem2(1);
+        st = new StringTokenizer(input.readLine());
+        int command = Integer.parseInt(st.nextToken());
+        if(command == 1){
+            long k = Long.parseLong(st.nextToken()) -1;
+            int [] arr = new int[n+1];
+            findOrder(k, 1, arr);
 
+            for(int i =1; i<= n; i++)
+                sb.append(arr[i]).append(" ");
+
+
+        } else {
+            // 주어진 숫자가 list의 몇번째 인덱스인지를 체크하고, 그 인덱스 * (n-1)!씩 하면서 더해주기.
+            long order = 0;
+            for(int i = 1; i <= n; i++){
+                int v = Integer.parseInt(st.nextToken());
+                int index = list.indexOf(v);
+                list.remove(index);
+                order += (factorial[n-i] * index);;
+            }
+            sb.append(order+1);
         }
 
         System.out.println(sb);
 
+
     }
 
-    static void problem1(int depth){
-        if (depth == n+1) {
+    static void findOrder(long k, int floor, int[] array){
+        if(floor == n){
+            array[floor] = list.get(0);
             return;
         }
-        int index = (int) (problem / factorial[n - depth]);
-        problem %= factorial[n - depth];
-        Integer cur = number.remove(index);
-        sb.append(cur).append(" ");
-        problem1(depth+1);
+
+        // k가 어디에 속하는지 체크하기
+        // k / (n-floor)!
+        int bound = (int)(k / factorial[ n - floor ]);
+        array[floor] = list.get(bound);
+        list.remove(bound);
+        long next = (k % factorial[ n - floor ]);
+        findOrder(next , floor+1, array);
     }
 
-    static void problem2(int depth){
-        if (depth == n+1) {
-            sb.append(answer+1);
-            return;
-        }
-        int index = number.indexOf(target[depth - 1]);
-        answer += factorial[n - depth] * index;
-        number.remove(index);
-        problem2(depth+1);
-
-    }
 
 }
