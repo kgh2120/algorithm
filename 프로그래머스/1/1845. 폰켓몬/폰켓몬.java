3a8850a0c4e1e6a203758b0cf97206295b6ffc05
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-02-18 16:33:07 +0900

    [level 1] Title: 폰켓몬, Time: 4.41 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,15 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
-    // N 마리가 주어지고 최대 N/2 마리를 선택할 때,
-    // 나올 수 있는 종류의 최대값.
-    // 종류로 나누고, 종류 vs N/2 중 더 작은 값을 고르면 될 듯.
     public int solution(int[] nums) {
+        int answer = 0;
+        
         Set<Integer> set = new HashSet<>();
         for(int num : nums)
             set.add(num);
-        int answer = Math.min(set.size(), nums.length / 2);
+        
+        answer = Math.min(set.size(), nums.length/2);
+        
         return answer;
     }
 }
