cbb6f2b5c82789324c51d7bbcf7ae1f02cb5df11
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-05-14 02:19:15 +0900

    [level 3] Title: 베스트앨범, Time: 1.23 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java#) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 2.12 ms
+메모리: 88 MB, 시간: 1.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 16일 19:30:13
+2025년 05월 14일 02:19:12
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,89 +1,92 @@
 import java.util.*;
 
+/**
+
+ 장르의 노래가 많을 때,
+ 장르 내에서 많이 재생된 노래일 때, -> plays
+ 고유 번호가 낮을 때, -> index
+ **/
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        
-        Map<String,Integer> genresIndexMap = new HashMap<>();
-        int listIndex = 0;
-        List<PlayCounter> nodes = new ArrayList<>();
-        Map<String, PriorityQueue<Node>> pqMap = new HashMap<>();
-        
-        for(int i = 0; i<genres.length; i++){
+
+        Map<String, Genre> map = new HashMap<>();
+        List<Genre> genreList = new ArrayList<>();
+
+        for(int i = 0; i < genres.length; i++){
+
             String genre = genres[i];
-            int play = plays[i];
-            
-            Integer genreIndex = genresIndexMap.get(genre);
-            if(genreIndex == null){
-                genreIndex = listIndex;
-                genresIndexMap.put(genre, listIndex++);
-                nodes.add(new PlayCounter(genre));
+
+            Genre g = map.get(genre);
+            if(g == null){
+                g = new Genre();
+                genreList.add(g);
+                map.put(genre, g);
             }
-            nodes.get(genreIndex).totalPlay += play;
-            
-            PriorityQueue pq = pqMap.get(genre);
-            if(pq == null){
-                pq = new PriorityQueue<Node>();
-                pqMap.put(genre,pq);
-            }
-            pq.add(new Node( play , i));
-            
+            g.count += plays[i];
+
+            g.songQueue.add(new Song(i, plays[i]));
+
         }
-        
-        Collections.sort(nodes);
-        List<Integer> ans = new ArrayList<>();
-        for(PlayCounter pc : nodes){
-            PriorityQueue<Node> pq = pqMap.get(pc.genres);
-            
-            int c = 0;
-            while(!pq.isEmpty() && c < 2){
-                c++;
-                ans.add(pq.poll().index);
-            }
+        List<Integer> list = new ArrayList<>();
+        Collections.sort(genreList);
+
+        for (Genre g : genreList) {
+            Collections.sort(g.songQueue);
+
+            int end = Math.min(2, g.songQueue.size());
+
+            for(int i = 0; i<end;i++)
+                list.add(g.songQueue.get(i).index);
         }
-        
-        answer = new int[ans.size()];
-        for(int i = 0; i<ans.size(); i++)
-            answer[i]= ans.get(i);
-        
-        
-        
+
+
+
+
+    int[] answer = new int[list.size()];
+
+        for(int i = 0; i<list.size(); i++)
+            answer[i] = list.get(i);
+
+
         return answer;
+}
+
+static class Genre implements Comparable<Genre>{
+    long count = 0L;
+    // Queue<Song> songQueue = new PriorityQueue<>();
+    List<Song> songQueue = new ArrayList<>();
+
+    @Override
+    public int compareTo(Genre o){
+
+        return Long.compare(this.count, o.count) * -1;
+
     }
-    
-    static class PlayCounter implements Comparable<PlayCounter>{
-        String genres;
-        int totalPlay;
-        
-        public PlayCounter(String genre){
-            genres = genre;
-        }
-        
-        @Override
-        public int compareTo(PlayCounter o){
-            return Integer.compare(this.totalPlay, o.totalPlay) * -1;
-        }
+
+}
+
+static class Song implements Comparable<Song>{
+    Genre genre;
+    int index;
+    int plays;
+
+    public Song(int i, int play){
+        this.index = i;
+        this.plays = play;
     }
-    
-    static class Node implements Comparable<Node>{
-        int plays;
-        int index;
-        
-        public Node(int p, int i){
-            plays = p;
-            index = i;
+
+    @Override
+    public int compareTo(Song o){
+
+
+        int comparePlay = Integer.compare(this.plays, o.plays) * -1;
+        if(comparePlay == 0){
+            return Integer.compare(this.index, o.index);
         }
-        
-        @Override
-        public int compareTo(Node o){
-            
-            int v = Integer.compare(this.plays, o.plays) * -1;
-            
-            if(v == 0){
-                return Integer.compare(this.index, o.index);
-            }
-            return v;
-            
-        }
+        return comparePlay;
+
+
     }
+}
 }
