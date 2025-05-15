21943139de9fdfa43ab5d2e68fbf0703749964a5
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-05-15 16:51:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.96 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 1.27 ms
+메모리: 79.4 MB, 시간: 0.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 16일 19:52:27
+2025년 05월 15일 16:51:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,45 +1,45 @@
 import java.util.*;
 
-
+// 넣을 때 트럭 무게랑 내리는 시간? 계산해서 넣어주면 될듯한데 
+// 내리는 시간이 들어가고 + bridge_length 잖아. 예시에서 1초에 7번이 올라가서 3초에 내리니까 
+// 그러면 내리는 시간, weight 넣고 하면 되는거지 뭐
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Queue<Truck> bridge = new ArrayDeque<>();
-        int curWeight = 0 ;
         
-        int time = 0;
-        for(int truckWeight : truck_weights){
-            time++;
-            if(!bridge.isEmpty() && bridge.peek().endTime == time){
-                curWeight -= bridge.poll().weight;
-            }
-                
+        int current = 0;
+        Queue<Node> b = new ArrayDeque<>();
+        int truckIndex = 0;
+        int answer = 1;
+        while(true){
             
-            // 못 넣을 때 대기
-            while(weight - curWeight < truckWeight){
-                Truck head = bridge.poll();
-                time = head.endTime;
-                curWeight -= head.weight;
-            } 
-            curWeight += truckWeight;
-            bridge.add(new Truck(time + bridge_length, truckWeight));
+            // 트럭을 넣을 수 있다면 넣고 1초 증가해
+            // 못넣으면 트럭 빼고 그 시간으로 이동해
+          
+            if(truckIndex < truck_weights.length && current + truck_weights[truckIndex] <= weight && b.size() < bridge_length){
+              int tw = truck_weights[truckIndex];
+                b.add(new Node(answer + bridge_length, tw));
+                current += tw;
+                answer++;
+                truckIndex++;
+            } else {
+                Node n = b.poll();
+                answer = Math.max(answer, n.out);
+                current -= n.weight;
+            }
+            
+            if(truckIndex == truck_weights.length && b.isEmpty())
+                break;
         }
         
-        while(!bridge.isEmpty())
-            answer = bridge.poll().endTime;
         
         return answer;
     }
-    
-    static class Truck{
-        int endTime;
+    static class Node{
+        int out;
         int weight;
-        
-        public Truck(int e, int w){
-            endTime = e;
-            weight = w;
+        public Node(int o, int w){
+            this.out = o;
+            this.weight = w;
         }
     }
-    
-    
 }
