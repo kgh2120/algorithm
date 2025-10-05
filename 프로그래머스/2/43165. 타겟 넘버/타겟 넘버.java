3f02725bb4d849d0ec05120452dcf390773783e3
김규현 <76154390+kgh2120@users.noreply.github.com>
2025-10-05 18:09:31 +0900

    [level 2] Title: 타겟 넘버, Time: 75.38 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,26 +3,25 @@ class Solution {
     public int solution(int[] numbers, int target) {
         int answer = 0;
         
+        int idx = 0;
         Queue<Integer> q = new ArrayDeque<>();
         q.add(0);
         
-        int index = 0;
-       for(int number : numbers){
+        for(int number : numbers){
             int size = q.size();
             while(size-->0){
-                int num = q.poll();
-                q.add(num + number);
-                q.add(num - number);
-            }
-          
+                int cur = q.poll();
+                q.add(cur + number);
+                q.add(cur - number);
+            }    
         }
         
         while(!q.isEmpty()){
             if(target == q.poll())
-               answer++; 
+                answer++;
         }
         
-    
+        
         
         return answer;
     }
